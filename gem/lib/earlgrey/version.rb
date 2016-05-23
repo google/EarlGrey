b2a9b9bdab4bd4f5a3e586884d3de7a2051714b1
bootstraponline <code@bootstraponline.com>
2016-05-23 09:48:36 -0400

    Update version number

--- gem/lib/earlgrey/version.rb
+++ gem/lib/earlgrey/version.rb
@@ -14,5 +14,5 @@
 #  limitations under the License.
 
 module EarlGrey
-  VERSION = '0.0.5'.freeze unless defined? ::EarlGrey::VERSION
+  VERSION = '0.0.7'.freeze unless defined? ::EarlGrey::VERSION
 end
