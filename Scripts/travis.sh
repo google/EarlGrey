f8224341dbee23dcd7d5851a1d161467a78561d3
Sundeep <sskhandp@google.com>
2016-06-29 16:30:34 -0700

    Refactoring travis yml and splitting the test commands into a separate script
    * Merging multiple -if/fi into single if/elif/fi
    
    Introducing another env var for unit tests
    
    * Moving xcodebuild test commands to its own travis.sh script
    
    * Using a new variable TYPE to differentiate different types of tests
    
    * making travis.sh executable
    
    * adding double quotes around
    
    * removing double brackets
    
    * fixing typo in FUNCTIONAL_SWIFT
    
    * making some fixes to make unit test work...lets hope it does
    
    * making some fixes to make unit test work...lets hope it does
    
    * fixing unit test project which included sources in the wrong target
    
    * uncomment all the env matrix
    
    * reverting UnitTests project
    
    * adding copyright to travis yml file

---
+++ Scripts/travis.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#  Copyright 2016 Google Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+set -euxo pipefail
+
+xcodebuild -version
+xcodebuild -showsdks
+
+if [ "${TYPE}" == "UNIT" ]; then
+  env NSUnbufferedIO=YES xcodebuild -project Tests/UnitTests/UnitTests.xcodeproj -scheme EarlGreyUnitTests -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
+elif [ "${TYPE}" == "RUBY" ]; then
+  cd gem;
+  bundle install --retry=3;
+  rake;
+elif [ "${TYPE}" == "FUNCTIONAL_SWIFT" ]; then
+  env NSUnbufferedIO=YES xcodebuild -project Tests/FunctionalTests/FunctionalTests.xcodeproj -scheme EarlGreyFunctionalSwiftTests -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
+elif [ "${TYPE}" == "FUNCTIONAL" ]; then
+  env NSUnbufferedIO=YES xcodebuild -project Tests/FunctionalTests/FunctionalTests.xcodeproj -scheme EarlGreyFunctionalTests -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
+else
+  echo "Unrecognized Type: ${TYPE}"
+  exit 1
+fi
+
