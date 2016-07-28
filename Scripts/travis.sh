a1e473291acaa9b8c38311198f265f03bc032e86
Sundeep <sskhandp@google.com>
2016-07-28 14:11:40 -0700

    Introducing retries if xcodebuild fails before tests are even run. (#202)
    * Adding better debugging for travis failures
    
    * removing other flavors for testing.
    
    * dump entire xcodebuild log on failure
    
    * forcing failures on travis
    
    * Making it easier to debug travis logs by redirecting all output to xcodebuild.log and adding a separate run for swift target of contribs project
    
    * adding retry logic to travis
    
    * fixing retry logic
    
    * fixing minor indentation with if
    
    * Check if test hasn't started before retrying
    
    * fixing syntax error in if statement
    
    * && => ||
    
    * introducing retval for grep
    
    * exit with proper status code when retries fail
    
    * addressing comments

--- Scripts/travis.sh
+++ Scripts/travis.sh
@@ -19,22 +19,60 @@ set -euxo pipefail
 xcodebuild -version
 xcodebuild -showsdks
 
-if [ "${TYPE}" == "UNIT" ]; then
-  env NSUnbufferedIO=YES xcodebuild -project Tests/UnitTests/UnitTests.xcodeproj -scheme EarlGreyUnitTests -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
-elif [ "${TYPE}" == "RUBY" ]; then
+# Runs xcodebuild retrying up to 3 times on failure to start testing (exit code 65).
+# The following arguments specified in the order below:
+#  $1 : .xcodeproj file
+#  $2 : scheme to run
+#
+# The output is prettified using xcpretty and redirected to xcodebuild.log for failure analysis.
+execute_xcodebuild() {
+  if [ -z ${1+x} ]; then
+    echo "first argument must be a valid .xcodeproj file"
+    exit 1
+  elif [ -z ${2+x} ]; then
+    echo "second argument must be a valid scheme"
+    exit 1
+  fi
+
+  retval_xcodebuild=0
+  for retry_attempts in {1..3}; do
+    # To retry on failure, disable exiting if command below fails.
+    set +e
+    env NSUnbufferedIO=YES xcodebuild -project ${1} -scheme ${2} -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | tee xcodebuild.log | xcpretty -s;
+    retval_xcodebuild=$?
+    # Even failed tests exit with code 65, check to ensure tests haven't started.
+    # We achieve that by looking for keyword "Test Suite" in xcodebuild.log.
+    # TODO: this behavior may change.
+    $(grep -q "Test Suite" xcodebuild.log)
+    retval_grep=$?
+    # Re-enable exiting for command failures.
+    set -e
+    if [[ ${retval_xcodebuild} -ne 65 ]] || [[ ${retval_grep} -eq 0 ]]; then
+      break
+    fi
+  done
+
+  if [[ ${retval_xcodebuild} -ne 0 ]]; then
+    exit ${retval_xcodebuild}
+  fi
+}
+
+if [ "${TYPE}" == "RUBY" ]; then
   rvm use 2.2.2;
   cd gem;
   bundle install --retry=3;
   rake;
+elif [ "${TYPE}" == "UNIT" ]; then
+  execute_xcodebuild Tests/UnitTests/UnitTests.xcodeproj EarlGreyUnitTests
 elif [ "${TYPE}" == "FUNCTIONAL_SWIFT" ]; then
-  env NSUnbufferedIO=YES xcodebuild -project Tests/FunctionalTests/FunctionalTests.xcodeproj -scheme EarlGreyFunctionalSwiftTests -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
+  execute_xcodebuild Tests/FunctionalTests/FunctionalTests.xcodeproj EarlGreyFunctionalSwiftTests
 elif [ "${TYPE}" == "FUNCTIONAL" ]; then
-  env NSUnbufferedIO=YES xcodebuild -project Tests/FunctionalTests/FunctionalTests.xcodeproj -scheme EarlGreyFunctionalTests -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
+  execute_xcodebuild Tests/FunctionalTests/FunctionalTests.xcodeproj EarlGreyFunctionalTests
 elif [ "${TYPE}" == "CONTRIB" ]; then
-  env NSUnbufferedIO=YES xcodebuild -project Demo/EarlGreyContribs/EarlGreyContribs.xcodeproj -scheme EarlGreyContribsTests -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
-  env NSUnbufferedIO=YES xcodebuild -project Demo/EarlGreyContribs/EarlGreyContribs.xcodeproj -scheme EarlGreyContribsSwiftTests -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
+  execute_xcodebuild Demo/EarlGreyContribs/EarlGreyContribs.xcodeproj EarlGreyContribsTests
+elif [ "${TYPE}" == "CONTRIB_SWIFT" ]; then
+  execute_xcodebuild Demo/EarlGreyContribs/EarlGreyContribs.xcodeproj EarlGreyContribsSwiftTests
 else
   echo "Unrecognized Type: ${TYPE}"
   exit 1
 fi
-
