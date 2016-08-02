4114cba5ef71c0b7123e641acb845ab5e840874c
sid-github <siddarthaj@google.com>
2016-08-01 23:38:01 -0700

    * Add Managed Object Context idling resource and improved dispatch queue idling resource (#204)
    * Adding safeguards in place for failures that are generated outside a valid test context

--- EarlGrey/Synchronization/GREYDispatchQueueIdlingResource.m
+++ EarlGrey/Synchronization/GREYDispatchQueueIdlingResource.m
@@ -16,148 +16,13 @@
 
 #import "Synchronization/GREYDispatchQueueIdlingResource.h"
 
-#include <dlfcn.h>
-#include <fishhook.h>
-#include <libkern/OSAtomic.h>
-
-#import "Common/GREYConfiguration.h"
-#import "Common/GREYDefines.h"
 #import "Common/GREYPrivate.h"
-#import "Synchronization/GREYUIThreadExecutor.h"
-
-/**
- *  A pointer to the original implementation of @c dispatch_after.
- */
-static void (*grey_original_dispatch_after)(dispatch_time_t when,
-                                            dispatch_queue_t queue,
-                                            dispatch_block_t block);
-/**
- *  A pointer to the original implementation of @c dispatch_async.
- */
-static void (*grey_original_dispatch_async)(dispatch_queue_t queue, dispatch_block_t block);
-/**
- *  A pointer to the original implementation of @c dispatch_sync.
- */
-static void (*grey_original_dispatch_sync)(dispatch_queue_t queue, dispatch_block_t block);
-
-/**
- *  Used to find idling resource corresponding to a dispatch queue, if one exists.
- */
-static NSMapTable *gDispatchQueueToIdlingResource;
-
-@interface GREYDispatchQueueIdlingResource ()
-
-- (void)grey_dispatchAfterCallWithTime:(dispatch_time_t)when block:(dispatch_block_t)block;
-- (void)grey_dispatchAsyncCallWithBlock:(dispatch_block_t)block;
-- (void)grey_dispatchSyncCallWithBlock:(dispatch_block_t)block;
-
-@end
-
-/**
- * @return The GREYDispatchQueueIdlingResource associated with @c queue or @c nil if there is none.
- */
-static GREYDispatchQueueIdlingResource *grey_getIdlingResourceForQueue(dispatch_queue_t queue) {
-  GREYDispatchQueueIdlingResource *resource = nil;
-  @synchronized(gDispatchQueueToIdlingResource) {
-    resource = [gDispatchQueueToIdlingResource objectForKey:queue];
-  }
-  return resource;
-}
-
-/**
- *  Overriden implementation of @c dispatch_after that calls into the idling resource, if one is
- *  found for the dispatch queue passed in.
- *
- *  @param when  Same as @c dispatch_after's @c when.
- *  @param queue Same as @c dispatch_after's @c queue.
- *  @param block Same as @c dispatch_after's @c block.
- */
-static void grey_dispatch_after(dispatch_time_t when,
-                                dispatch_queue_t queue,
-                                dispatch_block_t block) {
-  GREYDispatchQueueIdlingResource *resource = grey_getIdlingResourceForQueue(queue);
-  if (resource) {
-    [resource grey_dispatchAfterCallWithTime:when block:block];
-  } else {
-    grey_original_dispatch_after(when, queue, block);
-  }
-}
-
-/**
- *  Overriden implementation of @c dispatch_async that calls into the idling resource, if one is
- *  found for the dispatch queue passed in.
- *
- *  @param queue Same as @c dispatch_async @c queue.
- *  @param block Same as @c dispatch_async @c block.
- */
-static void grey_dispatch_async(dispatch_queue_t queue, dispatch_block_t block) {
-  GREYDispatchQueueIdlingResource *resource = grey_getIdlingResourceForQueue(queue);
-  if (resource) {
-    [resource grey_dispatchAsyncCallWithBlock:block];
-  } else {
-    grey_original_dispatch_async(queue, block);
-  }
-}
-
-/**
- *  Overriden implementation of @c dispatch_sync that calls into the idling resource, if one is
- *  found for the dispatch queue passed in.
- *
- *  @param queue Same as @c dispatch_sync @c queue.
- *  @param block Same as @c dispatch_sync @c block.
- */
-static void grey_dispatch_sync(dispatch_queue_t queue, dispatch_block_t block) {
-  GREYDispatchQueueIdlingResource *resource = grey_getIdlingResourceForQueue(queue);
-  if (resource) {
-    [resource grey_dispatchSyncCallWithBlock:block];
-  } else {
-    grey_original_dispatch_sync(queue, block);
-  }
-}
+#import "Synchronization/GREYDispatchQueueTracker.h"
 
 @implementation GREYDispatchQueueIdlingResource {
   NSString *_idlingResourceName;
-  __weak dispatch_queue_t _dispatchQueue;
-  __block int32_t _pendingBlocks;
+  GREYDispatchQueueTracker *_dispatchQueueTracker;
 }
-
-+ (void)load {
-  @autoreleasepool {
-    gDispatchQueueToIdlingResource = [NSMapTable weakToWeakObjectsMapTable];
-
-    dispatch_queue_t dummyQueue = dispatch_queue_create("GREYDummyQueue", DISPATCH_QUEUE_SERIAL);
-    NSAssert(dummyQueue, @"dummmyQueue must not be nil");
-
-    // Use dlsym to get the original pointer because of
-    // https://github.com/facebook/fishhook/issues/21
-    grey_original_dispatch_after = dlsym(RTLD_DEFAULT, "dispatch_after");
-    grey_original_dispatch_async = dlsym(RTLD_DEFAULT, "dispatch_async");
-    grey_original_dispatch_sync = dlsym(RTLD_DEFAULT, "dispatch_sync");
-    NSAssert(grey_original_dispatch_after, @"Pointer to dispatch_after must not be NULL");
-    NSAssert(grey_original_dispatch_async, @"Pointer to dispatch_async must not be NULL");
-    NSAssert(grey_original_dispatch_sync, @"Pointer to dispatch_sync must not be NULL");
-
-    // Rebind symbols dispatch_* to point to our own implementation.
-    struct rebinding rebindings[] = {
-      {"dispatch_after", grey_dispatch_after, NULL},
-      {"dispatch_async", grey_dispatch_async, NULL},
-      {"dispatch_sync", grey_dispatch_sync, NULL},
-    };
-    int failure = rebind_symbols(rebindings, sizeof(rebindings) / sizeof(rebindings[0]));
-    NSAssert(!failure, @"rebinding symbols failed");
-  }
-}
-
-#pragma mark - Methods Only For Testing
-
-/**
- *  @return the GREYIdlingResource for currently tracked dispatch @c queue.
- */
-+ (instancetype)grey_resourceForCurrentlyTrackedDispatchQueue:(dispatch_queue_t)queue {
-  return grey_getIdlingResourceForQueue(queue);
-}
-
-# pragma mark -
 
 + (instancetype)resourceWithDispatchQueue:(dispatch_queue_t)queue name:(NSString *)name {
   return [[GREYDispatchQueueIdlingResource alloc] initWithDispatchQueue:queue name:name];
@@ -170,17 +35,8 @@ static void grey_dispatch_sync(dispatch_queue_t queue, dispatch_block_t block) {
   self = [super init];
   if (self) {
     _idlingResourceName = [name copy];
-    _dispatchQueue = queue;
-
-    @synchronized(gDispatchQueueToIdlingResource) {
-      // TODO: Support a use case where a queue is tracked more than once.
-      id<GREYIdlingResource> resource = grey_getIdlingResourceForQueue(queue);
-      NSAssert(!resource, @"Queue \"%@\" is already being tracked by %@", queue, resource);
-      // Register this resource with dispatch queue to idling resource map.
-      [gDispatchQueueToIdlingResource setObject:self forKey:queue];
-    }
+    _dispatchQueueTracker = [GREYDispatchQueueTracker trackerForDispatchQueue:queue];
   }
-
   return self;
 }
 
@@ -195,46 +51,15 @@ static void grey_dispatch_sync(dispatch_queue_t queue, dispatch_block_t block) {
 }
 
 - (BOOL)isIdleNow {
-  if (!_dispatchQueue) {
+  // It is possible that all external references to the dispatch queue have been dropped, but that
+  // the queue is still kept active by enqueued blocks. In that case, the queue is a "zombie" and
+  // no longer a "live" queue. We want to track live and zombie queues, so we will only untrack the
+  // queue if it is no longer alive and it has no more enqueued blocks.
+  BOOL trackerIsIdle = [_dispatchQueueTracker isIdleNow];
+  if (trackerIsIdle && ![_dispatchQueueTracker isTrackingALiveQueue]) {
     [[GREYUIThreadExecutor sharedInstance] deregisterIdlingResource:self];
-    return YES;
   }
-  NSAssert(_pendingBlocks >= 0, @"_pendingBlocks must not be negative");
-  BOOL isIdle = OSAtomicCompareAndSwap32Barrier(0, 0, &_pendingBlocks);
-  return isIdle;
-}
-
-#pragma mark - Private
-
-- (void)grey_dispatchAfterCallWithTime:(dispatch_time_t)when block:(dispatch_block_t)block {
-  CFTimeInterval maxDelay = GREY_CONFIG_DOUBLE(kGREYConfigKeyDispatchAfterMaxTrackableDelay);
-  dispatch_time_t trackDelay = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(maxDelay * NSEC_PER_SEC));
-
-  if (trackDelay >= when) {
-    OSAtomicIncrement32Barrier(&_pendingBlocks);
-    grey_original_dispatch_after(when, _dispatchQueue, ^{
-      block();
-      OSAtomicDecrement32Barrier(&_pendingBlocks);
-    });
-  } else {
-    grey_original_dispatch_after(when, _dispatchQueue, block);
-  }
-}
-
-- (void)grey_dispatchAsyncCallWithBlock:(dispatch_block_t)block {
-  OSAtomicIncrement32Barrier(&_pendingBlocks);
-  grey_original_dispatch_async(_dispatchQueue, ^{
-    block();
-    OSAtomicDecrement32Barrier(&_pendingBlocks);
-  });
-}
-
-- (void)grey_dispatchSyncCallWithBlock:(dispatch_block_t)block {
-  OSAtomicIncrement32Barrier(&_pendingBlocks);
-  grey_original_dispatch_sync(_dispatchQueue, ^{
-    block();
-    OSAtomicDecrement32Barrier(&_pendingBlocks);
-  });
+  return trackerIsIdle;
 }
 
 @end
