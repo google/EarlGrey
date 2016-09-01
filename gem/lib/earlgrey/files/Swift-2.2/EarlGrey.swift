7070e1ab4abe5a63f6526c2f91ea651fbcbfcffc
sid-github <siddarthaj@google.com>
2016-08-31 19:28:29 -0700

    [ci skip]Synced external for 1.2.0 release (#246)

--- gem/lib/earlgrey/files/Swift-2.2/EarlGrey.swift
+++ gem/lib/earlgrey/files/Swift-2.2/EarlGrey.swift
@@ -81,7 +81,7 @@ public func GREYFail(reason: String) {
                                      details: "")
 }
 
-@available(*, deprecated=1.1.1, message="Please use GREYFAIL::withDetails instead.")
+@available(*, deprecated=1.2.0, message="Please use GREYFAIL::withDetails instead.")
 public func GREYFail(reason: String, details: String) {
   greyFailureHandler.handleException(GREYFrameworkException(name: kGREYAssertionFailedException,
     reason: reason),
