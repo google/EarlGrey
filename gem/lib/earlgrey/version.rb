fb4530a9c07730330f7d3094280bd6cf27c0ecff
Phil Song <phsong@google.com>
2016-11-10 11:52:10 -0800

    Update changes from internal source

--- gem/lib/earlgrey/version.rb
+++ gem/lib/earlgrey/version.rb
@@ -14,5 +14,5 @@
 #  limitations under the License.
 
 module EarlGrey
-  VERSION = '0.1.1'.freeze unless defined? ::EarlGrey::VERSION
+  VERSION = '0.1.0'.freeze unless defined? ::EarlGrey::VERSION
 end
