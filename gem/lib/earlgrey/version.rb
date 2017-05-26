dfa996b272ae84559180e725a265111410746c02
Aditya Atul Tirodkar <tirodkar@google.com>
2017-05-26 16:59:25 -0700

    Update Ruby Gem version to 0.2.2

--- gem/lib/earlgrey/version.rb
+++ gem/lib/earlgrey/version.rb
@@ -14,5 +14,5 @@
 #  limitations under the License.
 
 module EarlGrey
-  VERSION = '0.2.1'.freeze unless defined? ::EarlGrey::VERSION
+  VERSION = '0.2.2'.freeze unless defined? ::EarlGrey::VERSION
 end
