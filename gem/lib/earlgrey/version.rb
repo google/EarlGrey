bfe0cfbf2a5ecfb63cb450ce0c6580309e8b36ac
Aditya Atul Tirodkar <tirodkar@google.com>
2017-05-26 17:17:32 -0700

    Update the gem version to 1.9.3

--- gem/lib/earlgrey/version.rb
+++ gem/lib/earlgrey/version.rb
@@ -14,5 +14,5 @@
 #  limitations under the License.
 
 module EarlGrey
-  VERSION = '0.2.2'.freeze unless defined? ::EarlGrey::VERSION
+  VERSION = '1.9.3'.freeze unless defined? ::EarlGrey::VERSION
 end
