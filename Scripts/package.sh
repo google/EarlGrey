d0db87125dece25d62a115e4bb620f14c5d028cc
Hao Wu <wuhao.wise@gmail.com>
2017-08-09 11:01:37 -0700

    Add comments and license and fix style

--- Scripts/package.sh
+++ Scripts/package.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+#
+#  Copyright 2016 Google Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+#
+#  Build and archive EarlGrey.framework into EarlGrey.zip in the current running
+#  folder so it is ready to use for github release and cocoapods deployment.
+#
+
 SOURCE_DIR=$(dirname ${BASH_SOURCE[0]})
 CURRENT_DIR=$PWD
 : "${PROJECT_FILE_PATH:=${SOURCE_DIR}/../EarlGrey.xcodeproj}"
@@ -10,7 +31,8 @@ readonly PACKAGE_FILES="README.md CHANGELOG.md LICENSE"
 
 # Make a universal dynamic framework build.
 export OUTPUT_DIR=$OUTPUT_TMP_DIR/build
-(cd $SOURCE_DIR/.. && xcrun xcodebuild build -project "${PROJECT_FILE_PATH}" -target "Release")
+(cd $SOURCE_DIR/.. \
+  && xcrun xcodebuild build -project "${PROJECT_FILE_PATH}" -target "Release")
 
 # Copy files to the temp dir and zip.
 mkdir -p $OUTPUT_PACKAGE_DIR
@@ -19,7 +41,7 @@ mkdir -p $OUTPUT_PACKAGE_DIR
 # Symlink the framework so we don't need to copy.
 ln -s "$OUTPUT_DIR/EarlGrey.framework" $OUTPUT_PACKAGE_DIR/EarlGrey.framework
 
-(cd $OUTPUT_DIR/.. && \
-    zip -r -X "$CURRENT_DIR/${PACKAGE_NAME}.zip" "${PACKAGE_NAME}")
+(cd $OUTPUT_DIR/.. \
+  && zip -r -X "$CURRENT_DIR/${PACKAGE_NAME}.zip" "${PACKAGE_NAME}")
 
 rm -rf $OUTPUT_TMP_DIR
