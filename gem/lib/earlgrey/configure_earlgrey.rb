55eb898670fdc234046f6045d6aecab2c8e7e2c6
Hao Wu <wuhao.wise@gmail.com>
2017-08-08 19:39:10 -0700

    Fallback to 3.0 for later swift version as a backup

--- gem/lib/earlgrey/configure_earlgrey.rb
+++ gem/lib/earlgrey/configure_earlgrey.rb
@@ -378,7 +378,10 @@ module EarlGrey
       src_swift_name = 'EarlGrey.swift'
       src_swift = File.join(src_root, "Swift-#{swift_version}", src_swift_name)
 
-      raise 'Bundled swift missing' unless File.exist? src_swift
+      unless File.exist? src_swift
+        puts_magenta "Fallback to swift version 3.0 for #{swift_version}."
+        src_swift = File.join(src_root, "Swift-3.0", src_swift_name)
+      end
       dst_swift = File.join(dst_root, src_swift_name)
 
       FileUtils.copy src_swift, dst_swift
