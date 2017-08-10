486b22c33b0f8ed5c5036a69809434442e4f6f88
Hao Wu <wuhao.wise@gmail.com>
2017-08-10 00:18:58 -0700

    Address comments & style

--- Scripts/build-release.sh
+++ Scripts/build-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Copyright 2016 Google Inc.
+#  Copyright 2017 Google Inc.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -27,18 +27,19 @@
 mkdir -p "${OUTPUT_DIR}"
 
 xcrun xcodebuild build -project "${PROJECT_FILE_PATH}" -target "EarlGrey" \
-  -configuration $CONFIGURATION_NAME -sdk iphoneos CODE_SIGN_IDENTITY="" \
+  -configuration "${CONFIGURATION_NAME}" -sdk iphoneos CODE_SIGN_IDENTITY="" \
   CODE_SIGNING_REQUIRED=NO ENTITLEMENTS_REQUIRED=NO ONLY_ACTIVE_ARCH=NO \
-  CONFIGURATION_BUILD_DIR=$OUTPUT_DIR/iphoneos
+  CONFIGURATION_BUILD_DIR="${OUTPUT_DIR}/iphoneos"
 
 xcrun xcodebuild build \ -project "${PROJECT_FILE_PATH}" -target "EarlGrey" \
-  -configuration $CONFIGURATION_NAME -sdk iphonesimulator  \
-  CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ENTITLEMENTS_REQUIRED=NO \
-  ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR=$OUTPUT_DIR/iphonesimulator
+  -configuration "${CONFIGURATION_NAME}" -sdk iphonesimulator CODE_SIGN_IDENTITY="" \
+  CODE_SIGNING_REQUIRED=NO ENTITLEMENTS_REQUIRED=NO ONLY_ACTIVE_ARCH=NO \
+  CONFIGURATION_BUILD_DIR="${OUTPUT_DIR}/iphonesimulator"
 
-cp -r ${OUTPUT_DIR}/iphoneos/EarlGrey.framework $OUTPUT_DIR
+# Use the framework built for device as the final product and lipo it later.
+cp -r "${OUTPUT_DIR}/iphoneos/EarlGrey.framework" "${OUTPUT_DIR}"
 
-xcrun lipo -create ${OUTPUT_DIR}/iphoneos/EarlGrey.framework/EarlGrey \
-  ${OUTPUT_DIR}/iphonesimulator/EarlGrey.framework/EarlGrey \
-  -output $OUTPUT_DIR/EarlGrey.framework/EarlGrey
+xcrun lipo -create "${OUTPUT_DIR}/iphoneos/EarlGrey.framework/EarlGrey" \
+  "${OUTPUT_DIR}/iphonesimulator/EarlGrey.framework/EarlGrey" \
+  -output "${OUTPUT_DIR}/EarlGrey.framework/EarlGrey"
 
--- Scripts/package.sh
+++ Scripts/package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Copyright 2016 Google Inc.
+#  Copyright 2017 Google Inc.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -19,29 +19,30 @@
 #  folder so it is ready to use for github release and cocoapods deployment.
 #
 
-SOURCE_DIR=$(dirname ${BASH_SOURCE[0]})
-CURRENT_DIR=$PWD
+SOURCE_DIR="$(dirname ${BASH_SOURCE[0]})"
+CURRENT_DIR="${PWD}"
+
 : "${PROJECT_FILE_PATH:=${SOURCE_DIR}/../EarlGrey.xcodeproj}"
 : "${OUTPUT_DIR:=${SOURCE_DIR}/build}"
 
 readonly PACKAGE_NAME="EarlGrey"
 readonly OUTPUT_TMP_DIR=$(mktemp -d)
-readonly OUTPUT_PACKAGE_DIR=$OUTPUT_TMP_DIR/$PACKAGE_NAME
+readonly OUTPUT_PACKAGE_DIR="${OUTPUT_TMP_DIR}/${PACKAGE_NAME}"
 readonly PACKAGE_FILES="README.md CHANGELOG.md LICENSE"
 
 # Make a universal dynamic framework build.
-export OUTPUT_DIR=$OUTPUT_TMP_DIR/build
-(cd $SOURCE_DIR/.. \
+export OUTPUT_DIR="${OUTPUT_TMP_DIR}/build"
+(cd "${SOURCE_DIR}/.." \
   && xcrun xcodebuild build -project "${PROJECT_FILE_PATH}" -target "Release")
 
 # Copy files to the temp dir and zip.
-mkdir -p $OUTPUT_PACKAGE_DIR
-(cd $SOURCE_DIR/.. && cp $PACKAGE_FILES $OUTPUT_PACKAGE_DIR)
+mkdir -p "${OUTPUT_PACKAGE_DIR}"
+(cd "${SOURCE_DIR}/.." && cp "${PACKAGE_FILES}" "${OUTPUT_PACKAGE_DIR}")
 
 # Symlink the framework so we don't need to copy.
-ln -s "$OUTPUT_DIR/EarlGrey.framework" $OUTPUT_PACKAGE_DIR/EarlGrey.framework
+ln -s "${OUTPUT_DIR}/EarlGrey.framework" "${OUTPUT_PACKAGE_DIR}/EarlGrey.framework"
 
-(cd $OUTPUT_DIR/.. \
-  && zip -r -X "$CURRENT_DIR/${PACKAGE_NAME}.zip" "${PACKAGE_NAME}")
+(cd "${OUTPUT_DIR}/.." \
+  && zip -r -X "${CURRENT_DIR}/${PACKAGE_NAME}.zip" "${PACKAGE_NAME}")
 
-rm -rf $OUTPUT_TMP_DIR
+rm -rf "${OUTPUT_TMP_DIR}"
