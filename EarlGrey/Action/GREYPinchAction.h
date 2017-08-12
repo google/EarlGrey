5b849eda37278f78ba073af74efe938c8b5230b1
Sundeep <sskhandp@google.com>
2017-08-11 19:35:08 -0700

    don't use g3 import paths

--- EarlGrey/Action/GREYPinchAction.h
+++ EarlGrey/Action/GREYPinchAction.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2017 Google Inc.
+// Copyright 2016 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,8 +14,8 @@
 // limitations under the License.
 //
 
-#import "googlemac/iPhone/Shared/Testing/EarlGrey/v2/AppFramework/Action/GREYBaseAction.h"
-#import "googlemac/iPhone/Shared/Testing/EarlGrey/v2/CommonLib/GREYConstants.h"
+#import <EarlGrey/GREYBaseAction.h>
+#import <EarlGrey/GREYConstants.h>
 
 /**
  *  Error domain used for pinch related NSError objects.
