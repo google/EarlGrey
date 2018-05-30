dad1f204a18342c6044c4381c1d157dd09f84990
Yan Zhang <ynzhang@google.com>
2018-05-29 20:04:51 -0700

    Update version number in docs and plist for 1.14.0.

--- gem/lib/earlgrey/version.rb
+++ gem/lib/earlgrey/version.rb
@@ -14,5 +14,5 @@
 #  limitations under the License.
 
 module EarlGrey
-  VERSION = '1.13.1'.freeze unless defined? ::EarlGrey::VERSION
+  VERSION = '1.14.0'.freeze unless defined? ::EarlGrey::VERSION
 end
