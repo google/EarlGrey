537f0052ed24d25584369de1017536c52e06a15c
Elliott Heath <ellioh@amazon.com>
2019-02-05 18:38:14 -0800

    Updating the TestLib bridging header location for 'GREYElementMatcherBlock.h' to the current location.
    Fixes https://github.com/google/EarlGrey/issues/793 with a one liner fix.

--- TestLib/Swift/EarlGreySwiftBridgingHeader.h
+++ TestLib/Swift/EarlGreySwiftBridgingHeader.h
@@ -22,7 +22,7 @@
 #import "AppFramework/Action/GREYAction.h"
 #import "AppFramework/Action/GREYActionBlock.h"
 #import "AppFramework/Action/GREYActions.h"
-#import "AppFramework/Matcher/GREYElementMatcherBlock.h"
+#import "CommonLib/Matcher/GREYElementMatcherBlock.h"
 #import "CommonLib/DistantObject/GREYHostApplicationDistantObject.h"
 #import "CommonLib/Matcher/GREYMatcher.h"
 #import "TestLib/AlertHandling/XCTestCase+GREYSystemAlertHandler.h"
