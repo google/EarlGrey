b7aeed4c0bd9b3105aae797d96694138e3b64f46
bootstraponline <code@bootstraponline.com>
2019-04-30 00:08:10 -0400

    Update ErrorHandlingTest.m
    Fixes by tirodkar

--- Tests/Functional/Sources/ErrorHandlingTest.m
+++ Tests/Functional/Sources/ErrorHandlingTest.m
@@ -26,92 +26,92 @@
 @implementation ErrorHandlingTest
 
 - (void)testActionErrorContainsHierarchyForFailures {
-  NSError *error;
-  [[EarlGrey selectElementWithMatcher:grey_keyWindow()]
-      performAction:grey_scrollInDirection(kGREYDirectionUp, 10)
-              error:&error];
-  XCTAssertTrue([error.description containsString:@"|--<GREYTouchOverlayView"]);
+    NSError *error;
+    [[EarlGrey selectElementWithMatcher:grey_keyWindow()]
+     performAction:grey_scrollInDirection(kGREYDirectionUp, 10)
+     error:&error];
+    XCTAssertTrue([error.description containsString:@"|--<"]);
 }
 
 - (void)testAssertionErrorContainsHierarchyForFailures {
-  NSError *error;
-  [[EarlGrey selectElementWithMatcher:grey_keyWindow()] assertWithMatcher:grey_nil() error:&error];
-  XCTAssertTrue([error.description containsString:@"|--<GREYTouchOverlayView"]);
+    NSError *error;
+    [[EarlGrey selectElementWithMatcher:grey_keyWindow()] assertWithMatcher:grey_nil() error:&error];
+    XCTAssertTrue([error.description containsString:@"|--<"]);
 }
 
 - (void)testMatcherErrorContainsHierarchyForFailures {
-  NSError *error;
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"Invalid ID")] performAction:grey_tap()
-                                                                                   error:&error];
-  XCTAssertTrue([error.description containsString:@"|--<GREYTouchOverlayView"]);
-  error = nil;
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"Invalid ID")]
-      assertWithMatcher:grey_notNil()
-                  error:&error];
-  XCTAssertTrue([error.description containsString:@"|--<GREYTouchOverlayView"]);
+    NSError *error;
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"Invalid ID")] performAction:grey_tap()
+                                                                                     error:&error];
+    XCTAssertTrue([error.description containsString:@"|--<"]);
+    error = nil;
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"Invalid ID")]
+     assertWithMatcher:grey_notNil()
+     error:&error];
+    XCTAssertTrue([error.description containsString:@"|--<"]);
 }
 
 - (void)testIdlingResourceContainsOnlyOneHierarchyInstance {
-  [self openTestViewNamed:@"Animations"];
-  double originalTimeout = GREY_CONFIG_DOUBLE(kGREYConfigKeyInteractionTimeoutDuration);
-  [[GREYConfiguration sharedConfiguration] setValue:@(1)
-                                       forConfigKey:kGREYConfigKeyInteractionTimeoutDuration];
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(@"AnimationControl")]
-      performAction:grey_tap()];
-  NSError *error;
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(@"AnimationStatus")]
-      assertWithMatcher:grey_text(@"Paused")
-                  error:&error];
-
-  [[GREYConfiguration sharedConfiguration] setValue:@(originalTimeout)
-                                       forConfigKey:kGREYConfigKeyInteractionTimeoutDuration];
-  XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
-                 (NSUInteger)1);
+    [self openTestViewNamed:@"Animations"];
+    double originalTimeout = GREY_CONFIG_DOUBLE(kGREYConfigKeyInteractionTimeoutDuration);
+    [[GREYConfiguration sharedConfiguration] setValue:@(1)
+                                         forConfigKey:kGREYConfigKeyInteractionTimeoutDuration];
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(@"AnimationControl")]
+     performAction:grey_tap()];
+    NSError *error;
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(@"AnimationStatus")]
+     assertWithMatcher:grey_text(@"Paused")
+     error:&error];
+    
+    [[GREYConfiguration sharedConfiguration] setValue:@(originalTimeout)
+                                         forConfigKey:kGREYConfigKeyInteractionTimeoutDuration];
+    XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
+                   (NSUInteger)1);
 }
 
 - (void)testSimpleErrorDoesNotContainHierarchy {
-  NSError *error = GREYErrorMake(kGREYGenericErrorDomain, kGREYGenericErrorCode, @"Generic Error");
-  XCTAssertFalse([error.description containsString:@"GREYTouchOverlayView"]);
+    NSError *error = GREYErrorMake(kGREYGenericErrorDomain, kGREYGenericErrorCode, @"Generic Error");
+    XCTAssertFalse([error.description containsString:@""]);
 }
 
 - (void)testNestedErrorCreatedWithASimpleErrorDoesNotContainHierarchy {
-  NSError *nestedError =
-      GREYErrorMake(kGREYGenericErrorDomain, kGREYGenericErrorCode, @"Generic Nexted Error");
-  NSError *error = GREYErrorNestedMake(kGREYGenericErrorDomain, kGREYGenericErrorCode,
-                                       @"Generic Error", nestedError);
-
-  XCTAssertFalse([error.description containsString:@"GREYTouchOverlayView"]);
+    NSError *nestedError =
+    GREYErrorMake(kGREYGenericErrorDomain, kGREYGenericErrorCode, @"Generic Nexted Error");
+    NSError *error = GREYErrorNestedMake(kGREYGenericErrorDomain, kGREYGenericErrorCode,
+                                         @"Generic Error", nestedError);
+    
+    XCTAssertFalse([error.description containsString:@""]);
 }
 
 - (void)testErrorPopulatedInTheAppContainsOneHierarchy {
-  NSError *error = [[GREYHostApplicationDistantObject sharedInstance] errorPopulatedInTheApp];
-  XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
-                 (NSUInteger)1);
+    NSError *error = [[GREYHostApplicationDistantObject sharedInstance] errorPopulatedInTheApp];
+    XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
+                   (NSUInteger)1);
 }
 
 - (void)testNestedErrorPopulatedInTheAppContainsOneHierarchy {
-  NSError *error = [[GREYHostApplicationDistantObject sharedInstance] notedErrorPopulatedInTheApp];
-  XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
-                 (NSUInteger)1);
+    NSError *error = [[GREYHostApplicationDistantObject sharedInstance] notedErrorPopulatedInTheApp];
+    XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
+                   (NSUInteger)1);
 }
 
 - (void)testErrorCreatedInTheAppContainsOneHierarchy {
-  NSError *error = [[GREYHostApplicationDistantObject sharedInstance] errorPopulatedInTheApp];
-  XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
-                 (NSUInteger)1);
+    NSError *error = [[GREYHostApplicationDistantObject sharedInstance] errorPopulatedInTheApp];
+    XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
+                   (NSUInteger)1);
 }
 
 - (void)testNestedErrorInsideAnErrorWithHierarchyContainsOneHierarchy {
-  NSError *error =
-      [[GREYHostApplicationDistantObject sharedInstance] nestedErrorWithHierarchyCreatedInTheApp];
-  XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
-                 (NSUInteger)1);
+    NSError *error =
+    [[GREYHostApplicationDistantObject sharedInstance] nestedErrorWithHierarchyCreatedInTheApp];
+    XCTAssertEqual([self grey_hierarchyOccurrencesInErrorDescription:error.description],
+                   (NSUInteger)1);
 }
 
 - (void)testFormattingOfErrorCreatedInTheApp {
-  NSError *error = [[GREYHostApplicationDistantObject sharedInstance] errorCreatedInTheApp];
-  XCTAssertNoThrow([GREYError grey_nestedDescriptionForError:error],
-                   @"Failing on an error from the app did not throw an exception");
+    NSError *error = [[GREYHostApplicationDistantObject sharedInstance] errorCreatedInTheApp];
+    XCTAssertNoThrow([GREYError grey_nestedDescriptionForError:error],
+                     @"Failing on an error from the app did not throw an exception");
 }
 
 #pragma mark - Private
@@ -123,13 +123,13 @@
  *  @param description An NSString specifying an NSError's description.
  */
 - (NSUInteger)grey_hierarchyOccurrencesInErrorDescription:(NSString *)description {
-  NSRegularExpression *regex =
-      [NSRegularExpression regularExpressionWithPattern:@"GREYTouchOverlayView"
-                                                options:NSRegularExpressionCaseInsensitive
-                                                  error:nil];
-  return [regex numberOfMatchesInString:description
-                                options:0
-                                  range:NSMakeRange(0, [description length])];
+    NSRegularExpression *regex =
+    [NSRegularExpression regularExpressionWithPattern:@"<UIWindow"
+                                              options:NSRegularExpressionCaseInsensitive
+                                                error:nil];
+    return [regex numberOfMatchesInString:description
+                                  options:0
+                                    range:NSMakeRange(0, [description length])];
 }
 
 @end
