cae75cb112c81bb77881a7cb266c0f6236619b57
Aditya Atul Tirodkar <tirodkar@google.com>
2019-05-24 10:46:31 -0700

    Add GREYAppState Import in the Swift file

--- Tests/Functional/Sources/Swift/SwiftTestBridgingHeader.h
+++ Tests/Functional/Sources/Swift/SwiftTestBridgingHeader.h
@@ -17,6 +17,7 @@
 #import "AppFramework/Action/GREYAction.h"
 #import "AppFramework/Action/GREYActionBlock.h"
 #import "AppFramework/Action/GREYActions.h"
+#import "CommonLib/Config/GREYAppState.h"
 #import "CommonLib/Config/GREYConfiguration.h"
 #import "CommonLib/DistantObject/GREYHostApplicationDistantObject.h"
 #import "CommonLib/Matcher/GREYMatcher.h"
