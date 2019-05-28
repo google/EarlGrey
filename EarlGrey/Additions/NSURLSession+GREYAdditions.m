b9c2eb92de874ba887276a7b9d54819dafdae57e
Leo Natan <leo.natan@outlook.com>
2019-05-28 18:51:20 +0300

    Fix further NSURLSession swizzling woes
    Following #710, a new issue was discovered when Earl Grey is linked together with New Relic. This PR fixes the bug by actually selecting the correct `delegate` object and querying correctly if that object responds to the original selector.
    
    Closes #839

--- EarlGrey/Additions/NSURLSession+GREYAdditions.m
+++ EarlGrey/Additions/NSURLSession+GREYAdditions.m
@@ -54,19 +54,21 @@ typedef void (^GREYTaskCompletionBlock)(NSData *data, NSURLResponse *response, N
   // Swizzle the session delegate class if not yet done.
   id delegate = self.delegate;
   SEL originalSel = @selector(URLSession:task:didCompleteWithError:);
-  Class delegateClass =
-      [[delegate forwardingTargetForSelector:originalSel] class] ?: [delegate class];
-
+  // This is to solve issues where there is a proxy delegate instead of the original instance (e.g. TrustKit, New Relic).
+  // It responds YES to `respondsToSelector:` but `class_getInstanceMethod` returns nil.
+  // We attempt to follow the forwarding targets for the selector, if any exists.
+  id nextForwardingDelegate = nil;
+  while((nextForwardingDelegate = [delegate forwardingTargetForSelector:originalSel]) != nil)
+  {
+    delegate = nextForwardingDelegate;
+  }
+  Class delegateClass = [delegate class];
+  
   if (![delegateClass instancesRespondToSelector:swizzledSel]) {
-    SEL originalSel = @selector(URLSession:task:didCompleteWithError:);
     // If delegate does not exists or delegate does not implement the delegate method, then this
     // request need not be tracked as its completion/failure does not trigger any delegate
     // callbacks.
     if ([delegate respondsToSelector:originalSel]) {
-      // This is to solve issues where there is a proxy delegate instead of the original instance (e.g. TrustKit).
-      // It responds YES to `respondsToSelector:` but `class_getInstanceMethod` returns nil.
-      // We attempt to take the forwarding target for the selector, if it exists.
-      delegateClass = [[delegate forwardingTargetForSelector:originalSel] class] ?: delegateClass;
       Class selfClass = [self class];
       // Double-checked locking to prevent multiple swizzling attempts of the same class.
       @synchronized(selfClass) {
