b3c355935eb141b5937c569c6628046f11af0360
Brett <brett.fazio@hotmail.com>
2019-06-21 14:06:40 -0700

    Delete tearDown method.

--- Demo/EarlGreyExample/EarlGreyExampleUITests/EarlGreyExampleUITests.m
+++ Demo/EarlGreyExample/EarlGreyExampleUITests/EarlGreyExampleUITests.m
@@ -44,10 +44,6 @@
   [[[XCUIApplication alloc] init] launch];
 }
 
-- (void)tearDown {
-  // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
 - (void)testBasicSelection {
   // Select the button with Accessibility ID "clickMe".
   [EarlGrey selectElementWithMatcher:grey_accessibilityID(@"ClickMe")];
