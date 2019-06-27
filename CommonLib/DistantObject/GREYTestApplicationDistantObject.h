42f084a0ac57fbc37d35999bd8414a438728efc2
Brett <brettfazio@google.com>
2019-06-26 18:18:14 -0700

    modular imports

--- CommonLib/DistantObject/GREYTestApplicationDistantObject.h
+++ CommonLib/DistantObject/GREYTestApplicationDistantObject.h
@@ -17,17 +17,17 @@
 #import <Foundation/Foundation.h>
 
 // Use modular imports for CocoaPods release.
-/*
- #import <eDistantObject/EDOClientService.h>
- #import <eDistantObject/EDOHostService.h>
- #import <eDistantObject/EDOServicePort.h>
-*/
-// End CocoaPods modular imports
 
+#import <eDistantObject/EDOClientService.h>
+#import <eDistantObject/EDOHostService.h>
+#import <eDistantObject/EDOServicePort.h>
+
+// End CocoaPods modular imports
+/*
 #import "EDOClientService.h"
 #import "EDOHostService.h"
 #import "EDOServicePort.h"
-
+*/
 NS_ASSUME_NONNULL_BEGIN
 
 /**
