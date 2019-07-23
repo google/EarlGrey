bf606a29dd01b4aa0f6f9f5e7d2efd89e41885e8
Mobile DevX Robot <mobile-devx-github-bot@google.com>
2019-07-23 15:48:39 -0700

    Minor fix in doc and formatting.
    PiperOrigin-RevId: 259628301

--- CommonLib/GREYDiagnosable.h
+++ CommonLib/GREYDiagnosable.h
@@ -19,7 +19,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- *  @return Prefixed diagnosticsID for "core" matcher, action, and assertion.
+ *  @return Prefixed @c diagnosticsID for "core" matcher, action, and assertion.
  */
 NSString *GREYCorePrefixedDiagnosticsID(NSString *diagnosticsID);
 
