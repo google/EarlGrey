83f353201ce4d9eda7dcda537c960d0037ca63ab
Kartheek Jooluri <joolurik@amazon.com>
2020-06-17 16:56:22 -0700

    Revert "Added downloaded sub-modules into the project."
    This reverts commit a3e0810b3aad2a5420867ba4831e8f397e94a8fe.

--- Submodules/eDistantObject/Channel/Sources/EDOChannelErrors.m
+++
@@ -1,5 +0,0 @@
-#import "Channel/Sources/EDOChannelErrors.h"
-
-NSString *const EDOChannelErrorDomain = @"com.google.edo.channel";
-
-NSErrorUserInfoKey const EDOChannelPortKey = @"EDOErrorChannelPort";
