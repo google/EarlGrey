0f61ac77a969ffc9bc2a7a362bfe5b2e35e488ef
Chan Park <chanheepark@google.com>
2020-06-22 18:24:32 -0700

    Implement GREYErrorFormatter.
    GREYErrorFormatter is a class that converts error to a human readable string.
    
    PiperOrigin-RevId: 317776969

---
+++ AppFramework/Error/GREYErrorFormatter.h
@@ -0,0 +1,63 @@
+//
+// Copyright 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import <Foundation/Foundation.h>
+
+@class GREYError;
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * Formats the description to be printed for a GREYError.
+ */
+@interface GREYErrorFormatter : NSObject
+
+/**
+ * @note This is the formatted description of the GREYError itself, so it does not include the
+ *       hierarchy, screenshots, or stack trace - those are populated on the test side, typically in
+ *       the failure handler.
+ *
+ * @param error The GREYError being formatted.
+ *
+ * @return The full description of the error including its nested errors suitable for output to the
+ *         user, depending on the error's code and domain.
+ */
++ (NSString *)formattedDescriptionForError:(GREYError *)error;
+
+/**
+ * Determines whether this error's code and domain are supported by GREYErrorFormatter. If @c NO,
+ * then default to using the existing dictionary object formatting.
+ *
+ * @note This is temporary until all errors are using GREYErrorFormatter.
+ *
+ * @param error The GREYError being formatted.
+ *
+ * @return @c YES if the new formatting based on the code and domain should be used for this error.
+ */
+BOOL GREYShouldUseErrorFormatterForError(GREYError *error);
+
+/**
+ * Determines whether a failure handler's details should use GREYErrorFormatter.
+ *
+ * This is temporary until all errors are using GREYErrorFormatter.
+ *
+ * @return @c YES if the new formatting should be used for the failure handler, else @c NO.
+ */
+BOOL GREYShouldUseErrorFormatterForDetails(NSString *failureHandlerDetails);
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AppFramework/Error/GREYErrorFormatter.m
@@ -0,0 +1,141 @@
+//
+// Copyright 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import "GREYErrorFormatter.h"
+
+#import "GREYInteraction.h"
+#import "GREYFatalAsserts.h"
+#import "GREYError+Private.h"
+#import "GREYError.h"
+#import "GREYErrorConstants.h"
+#import "GREYObjectFormatter.h"
+#import "NSError+GREYCommon.h"
+
+#pragma mark - UI Hierarchy Keys
+
+static NSString *const kHierarchyWindowLegendKey = @"[Window 1]";
+static NSString *const kHierarchyAcessibilityLegendKey = @"[AX]";
+static NSString *const kHierarchyUserInteractionEnabledLegendKey = @"[UIE]";
+static NSString *const kHierarchyBackWindowKey = @"Back-Most Window";
+static NSString *const kHierarchyAccessibilityKey = @"Accessibility";
+static NSString *const kHierarchyUserInteractionEnabledKey = @"User Interaction Enabled";
+static NSString *const kHierarchyLegendKey = @"Legend";
+static NSString *const kHierarchyHeaderKey = @"UI Hierarchy (ordered by wind"
+                                             @"ow level, back to front):\n";
+static NSString *const kErrorPrefix = @"EarlGrey Encountered an Error:";
+
+#pragma mark - GREYErrorFormatter
+
+@implementation GREYErrorFormatter
+
+#pragma mark - Public Methods
+
++ (NSString *)formattedDescriptionForError:(GREYError *)error {
+  if (GREYShouldUseErrorFormatterForError(error)) {
+    return LoggerDescription(error);
+  }
+  return [GREYObjectFormatter formatDictionary:[error grey_descriptionDictionary]
+                                        indent:kGREYObjectFormatIndent
+                                     hideEmpty:YES
+                                      keyOrder:nil];
+}
+
+#pragma mark - Public Functions
+
+BOOL GREYShouldUseErrorFormatterForError(GREYError *error) {
+  return [error.domain isEqualToString:kGREYInteractionErrorDomain] &&
+         error.code == kGREYInteractionElementNotFoundErrorCode;
+}
+
+BOOL GREYShouldUseErrorFormatterForDetails(NSString *failureHandlerDetails) {
+  return [failureHandlerDetails hasPrefix:kErrorPrefix];
+}
+
+#pragma mark - Static Functions
+
+static NSString *FormattedHierarchy(NSString *hierarchy) {
+  if (!hierarchy) {
+    return nil;
+  }
+  NSMutableArray<NSString *> *logger = [[NSMutableArray alloc] init];
+  [logger addObject:kHierarchyHeaderKey];
+  NSString *windowLegend = kHierarchyWindowLegendKey;
+  NSString *axLegend = kHierarchyAcessibilityLegendKey;
+  NSString *uieLegend = kHierarchyUserInteractionEnabledLegendKey;
+  NSDictionary<NSString *, NSString *> *legendLabels = @{
+    windowLegend : kHierarchyBackWindowKey,
+    axLegend : kHierarchyAccessibilityKey,
+    uieLegend : kHierarchyUserInteractionEnabledKey
+  };
+  NSArray<NSString *> *keyOrder = @[ windowLegend, axLegend, uieLegend ];
+  NSString *legendDescription = [GREYObjectFormatter formatDictionary:legendLabels
+                                                               indent:kGREYObjectFormatIndent
+                                                            hideEmpty:NO
+                                                             keyOrder:keyOrder];
+  [logger
+      addObject:[NSString stringWithFormat:@"%@: %@\n", kHierarchyLegendKey, legendDescription]];
+  [logger addObject:hierarchy];
+  return [logger componentsJoinedByString:@"\n"];
+}
+
+static NSString *LoggerDescription(GREYError *error) {
+  NSMutableString *logger = [[NSMutableString alloc] init];
+  // Flag checked by GREYErrorFormatted(details, screenshotPaths).
+  // TODO(wsaid): remove this when the GREYErrorFormatted(details, screenshotPaths) is removed
+  [logger appendString:kErrorPrefix];
+  NSString *exceptionReason = error.localizedDescription;
+  if (exceptionReason) {
+    [logger appendFormat:@"\n\n%@", exceptionReason];
+  }
+
+  NSString *recoverySuggestion = error.userInfo[kErrorDetailRecoverySuggestionKey];
+  if (recoverySuggestion) {
+    [logger appendFormat:@"\n\n%@", recoverySuggestion];
+  }
+
+  NSString *elementMatcher = error.userInfo[kErrorDetailElementMatcherKey];
+  if (elementMatcher) {
+    [logger appendFormat:@"\n\n%@:\n%@", kErrorDetailElementMatcherKey, elementMatcher];
+  }
+
+  NSString *assertionCriteria = error.userInfo[kErrorDetailAssertCriteriaKey];
+  if (assertionCriteria) {
+    [logger appendFormat:@"\n\n%@: %@", kErrorDetailAssertCriteriaKey, assertionCriteria];
+  }
+  NSString *actionCriteria = error.userInfo[kErrorDetailActionNameKey];
+  if (actionCriteria) {
+    [logger appendFormat:@"\n\n%@: %@", kErrorDetailActionNameKey, actionCriteria];
+  }
+
+  NSString *searchActionInfo = error.userInfo[kErrorDetailSearchActionInfoKey];
+  if (searchActionInfo) {
+    [logger appendFormat:@"\n\n%@\n%@", kErrorDetailSearchActionInfoKey, searchActionInfo];
+  }
+
+  NSString *nestedError = error.nestedError.description;
+  if (nestedError) {
+    [logger appendFormat:@"\n\nUnderlying Error:\n%@", nestedError];
+  }
+
+  NSString *hierarchy = FormattedHierarchy(error.appUIHierarchy);
+  if (hierarchy) {
+    [logger appendFormat:@"\n\n%@", hierarchy];
+  }
+
+  return [NSString stringWithFormat:@"%@\n", logger];
+}
+
+@end
