a1c3b1271dcd33d15f2df2324a3ed662ef4c02d5
Aditya Atul Tirodkar <tirodkar@google.com>
2020-07-16 17:50:18 -0700

    Remove UIWebView loading in GREYAutomationSetup.
    PiperOrigin-RevId: 321683943

--- AppFramework/AutomationSetup/GREYAutomationSetup.m
+++ AppFramework/AutomationSetup/GREYAutomationSetup.m
@@ -60,16 +60,6 @@ static GREYSignalHandler gPreviousSignalHandlers[kNumSignals];
 @implementation GREYAutomationSetup
 
 + (void)load {
-// TODO(b/135635949): Remove this after fixing failing test.
-#if !defined(__IPHONE_12_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_12_0
-  // TODO: Perform a scan of UIWebView usage and deprecate if possible. // NOLINT
-  // It seems that in +[UIWebView initialize], it performs something that needs to be done on the
-  // thread; this causes crashes when we try to fetch +[UIWebView class] from a background thread,
-  // where our actions are being performed. We proactively load the class and trigger the message
-  // +initialize to be sent on UIWebView on the main thread here.
-  [UIWebView class];
-#endif
-
   GREYSetupCrashHandlers();
   // Force software keyboard.
   [[UIKeyboardImpl sharedInstance] setAutomaticMinimizationEnabled:NO];
