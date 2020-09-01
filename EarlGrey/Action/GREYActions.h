abf7477740ef0af7a263f81db1c3e307d5a15de3
Xavier Jurado <xavier.jurado@gmail.com>
2020-09-01 15:28:45 +0200

    Bypass compiler documentation warning due to escaped character

--- EarlGrey/Action/GREYActions.h
+++ EarlGrey/Action/GREYActions.h
@@ -317,7 +317,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  @param text The text to be typed. For Objective-C, backspace is supported by using "\b" in the
  *              string and "\u{8}" in Swift strings. Return key is supported with "\n".
- *              For Example: @"Helpo\b\bloWorld" will type HelloWorld in Objective-C.
+ *              For Example: @"Help\bloWorld" will type HelloWorld in Objective-C.
  *                           "Helpo\u{8}\u{8}loWorld" will type HelloWorld in Swift.
  *
  *  @return A GREYAction to type a specific text string in a text field.
