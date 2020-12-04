1fbab772af7ae7c73ce51a3e686975b07398f153
Aditya Atul Tirodkar <tirodkar@google.com>
2020-12-04 08:09:02 -0800

    #fixes https://github.com/google/EarlGrey/pull/1515
    PiperOrigin-RevId: 345679792

--- CommonLib/DistantObject/GREYHostApplicationDistantObject.h
+++ CommonLib/DistantObject/GREYHostApplicationDistantObject.h
@@ -19,8 +19,11 @@
 // Use modular imports for CocoaPods release.
 // #import <eDistantObject/EDOClientService.h>
 // End CocoaPods modular imports
-
+#if COCOAPODS
+#import <eDistantObject/EDOClientService.h>
+#else
 #import "EDOClientService.h"
+#endif  // COCOAPODS
 
 @class EDOHostService;
 
