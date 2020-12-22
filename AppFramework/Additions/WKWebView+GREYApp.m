a82bb6d39ffb196e432361cc6139b8ce6c95d32e
Chan Park <chanheepark@google.com>
2020-12-21 17:30:09 -0800

    Add swizzled implementation for WKWebView class.
    PiperOrigin-RevId: 348554442

---
+++ AppFramework/Additions/WKWebView+GREYApp.m
@@ -0,0 +1,111 @@
+//
+// Copyright 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#if TARGET_OS_IOS
+#import "WKWebView+GREYApp.h"
+
+#include <objc/runtime.h>
+
+#import "GREYFatalAsserts.h"
+#import "GREYSwizzler.h"
+
+@implementation WKWebView (GREYApp)
+
++ (void)load {
+  GREYSwizzler *swizzler = [[GREYSwizzler alloc] init];
+  BOOL swizzleSuccess = [swizzler swizzleClass:self
+                         replaceInstanceMethod:@selector(setNavigationDelegate:)
+                                    withMethod:@selector(greyswizzled_setNavigationDelegate:)];
+  GREYFatalAssertWithMessage(swizzleSuccess, @"Cannot swizzle setNavigationDelegate:");
+
+  swizzleSuccess = [swizzler swizzleClass:self
+                    replaceInstanceMethod:@selector(navigationDelegate)
+                               withMethod:@selector(greyswizzled_navigationDelegate)];
+  GREYFatalAssertWithMessage(swizzleSuccess, @"Cannot swizzle navigationDelegate:");
+
+  swizzleSuccess = [swizzler swizzleClass:self
+                    replaceInstanceMethod:@selector(loadRequest:)
+                               withMethod:@selector(greyswizzled_loadRequest:)];
+  GREYFatalAssertWithMessage(swizzleSuccess, @"Cannot swizzle setNavigationDelegate:");
+
+  swizzleSuccess = [swizzler swizzleClass:self
+                    replaceInstanceMethod:@selector(loadFileURL:allowingReadAccessToURL:)
+                               withMethod:@selector(greyswizzled_loadFileURL:
+                                                     allowingReadAccessToURL:)];
+  GREYFatalAssertWithMessage(swizzleSuccess,
+                             @"Cannot swizzle loadFileURL:allowingReadAccessToURL:");
+
+  // Method loadHTMLString:baseURL: invokes the following method internally. Therefore, it does not
+  // need swizzling.
+  swizzleSuccess =
+      [swizzler swizzleClass:self
+          replaceInstanceMethod:@selector(loadData:MIMEType:characterEncodingName:baseURL:)
+                     withMethod:@selector(greyswizzled_loadData:
+                                                       MIMEType:characterEncodingName:baseURL:)];
+  GREYFatalAssertWithMessage(swizzleSuccess,
+                             @"Cannot swizzle loadData:MIMEType:characterEncodingName:baseURL:");
+
+  swizzleSuccess = [swizzler swizzleClass:self
+                    replaceInstanceMethod:@selector(reload)
+                               withMethod:@selector(greyswizzled_reload)];
+  GREYFatalAssertWithMessage(swizzleSuccess, @"Cannot swizzle reload");
+
+  swizzleSuccess = [swizzler swizzleClass:self
+                    replaceInstanceMethod:@selector(reloadFromOrigin)
+                               withMethod:@selector(greyswizzled_reloadFromOrigin)];
+  GREYFatalAssertWithMessage(swizzleSuccess, @"Cannot swizzle reloadFromOrigin");
+}
+
+#pragma mark - Swizzled Implementation
+
+- (void)greyswizzled_setNavigationDelegate:(id)navigationDelegate {
+  INVOKE_ORIGINAL_IMP1(void, @selector(greyswizzled_setNavigationDelegate:), navigationDelegate);
+}
+
+- (id<WKNavigationDelegate>)greyswizzled_navigationDelegate {
+  return INVOKE_ORIGINAL_IMP(id<WKNavigationDelegate>, @selector(greyswizzled_navigationDelegate));
+}
+
+- (WKNavigation *)greyswizzled_loadRequest:(NSURLRequest *)request {
+  return INVOKE_ORIGINAL_IMP1(WKNavigation *, @selector(greyswizzled_loadRequest:), request);
+}
+
+- (WKNavigation *)greyswizzled_loadFileURL:(NSURL *)URL
+                   allowingReadAccessToURL:(NSURL *)readAccessURL {
+  return INVOKE_ORIGINAL_IMP2(WKNavigation *,
+                              @selector(greyswizzled_loadFileURL:allowingReadAccessToURL:), URL,
+                              readAccessURL);
+}
+
+- (WKNavigation *)greyswizzled_loadData:(NSData *)data
+                               MIMEType:(NSString *)MIMEType
+                  characterEncodingName:(NSString *)characterEncodingName
+                                baseURL:(NSURL *)baseURL {
+  return INVOKE_ORIGINAL_IMP4(
+      WKNavigation *, @selector(greyswizzled_loadData:MIMEType:characterEncodingName:baseURL:),
+      data, MIMEType, characterEncodingName, baseURL);
+}
+
+- (WKNavigation *)greyswizzled_reload {
+  return INVOKE_ORIGINAL_IMP(WKNavigation *, @selector(greyswizzled_reload));
+}
+
+- (WKNavigation *)greyswizzled_reloadFromOrigin {
+  return INVOKE_ORIGINAL_IMP(WKNavigation *, @selector(greyswizzled_reloadFromOrigin));
+}
+
+@end
+#endif
