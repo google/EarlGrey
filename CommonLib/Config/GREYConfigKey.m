5e41bd95c5cc35c40842e5cd2ca4a70a2fb83016
Aditya Atul Tirodkar <tirodkar@google.com>
2021-01-15 22:12:27 -0800

    CopyCat Config Name
    PiperOrigin-RevId: 352138380

--- CommonLib/Config/GREYConfigKey.m
+++ CommonLib/Config/GREYConfigKey.m
@@ -32,4 +32,4 @@ GREYConfigKey const kGREYConfigKeyDispatchAfterMaxTrackableDelay =
 GREYConfigKey const kGREYConfigKeyDelayedPerformMaxTrackableDuration =
     @"GREYConfigKeyDelayedPerformMaxTrackableDuration";
 GREYConfigKey const kGREYConfigKeyArtifactsDirLocation = @"GREYConfigKeyArtifactsDirLocation";
-GREYConfigKey const kGREYConfigKeyIgnoreAppStates = @"GREYConfigKeyIgnoreAppStates";
+GREYConfigKey const kGREYConfigKeyIgnoreAppStates = @"kGREYConfigKeyIgnoreAppStates";
