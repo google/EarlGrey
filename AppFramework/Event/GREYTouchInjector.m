4953ead560993e0f3242e981ec593014db96c614
Albert Wang <albertbow@google.com>
2022-02-03 13:07:06 -0800

    Correctly throw exceptions in users' view delegate.
    Touch injector captures users' exceptions in try-catch block to release resources, but it fails to re-throw users' exceptions. This change fixes the re-throw logic.
    
    PiperOrigin-RevId: 426220252

--- AppFramework/Event/GREYTouchInjector.m
+++ AppFramework/Event/GREYTouchInjector.m
@@ -97,7 +97,7 @@ static const NSTimeInterval kTouchInjectFramerateInv = 1 / 120.0;
   __weak __block void (^weakTouchProcessBlock)(void);
   void (^touchProcessBlock)(void) = ^{
     void (^strongTouchProcessBlock)(void) = weakTouchProcessBlock;
-    // If the parent method times out and returns, it will effectively stop this block execution
+    // If the parent method times out and returns, it will effectively kill this block execution
     // by releasing the strong reference.
     if (!strongTouchProcessBlock) {
       return;
@@ -224,6 +224,7 @@ static const NSTimeInterval kTouchInjectFramerateInv = 1 / 120.0;
             ongoingTouches:(NSMutableArray<UITouch *> *)ongoingTouches
                  exception:(NSException **)exception {
   GREYFatalAssertMainThread();
+  id injectionException;
   UITouchesEvent *event = [[UIApplication sharedApplication] _touchesEvent];
   [self grey_updateUITouchObjectsFromTouchInfo:touchInfo
                                 ongoingTouches:ongoingTouches
@@ -281,12 +282,12 @@ static const NSTimeInterval kTouchInjectFramerateInv = 1 / 120.0;
     @autoreleasepool {
       [[UIApplication sharedApplication] sendEvent:event];
     }
-  } @catch (id injectionException) {
-    *exception = injectionException;
+  } @catch (id exception) {
+    injectionException = exception;
   } @finally {
     [event _setHIDEvent:NULL];
   }
-  return !*exception;
+  return !injectionException;
 }
 
 /**
