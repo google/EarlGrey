d53349bb89da1145a0438d771aadb175dc420082
Yanni Wang <wy19900729@gmail.com>
2022-03-31 10:51:59 +0800

    Fix wrong path in Scripts/download_deps.sh

--- Scripts/download_deps.sh
+++ Scripts/download_deps.sh
@@ -2,5 +2,5 @@ if [ -d "Submodules" ]; then
     echo "Already cloned submodules. Delete the submodules directory to re-clone submodules."
 else
     echo "Submodules does not exist, cloning submodules."
-    git clone https://github.com/google/eDistantObject Submodules/eDistantObject
+    git clone https://github.com/google/eDistantObject ../Submodules/eDistantObject
 fi
