f8ae2c802d58751d8fcaaffa719725de2f0af543
Albert Wang <albertbow@google.com>
2023-02-01 17:22:15 -0800

    Rollback of:
    Convert 1-bit signed char bitfield _firstTouchForView to 1-bit unsigned integer bitfield
    
    Fixes -Wsingle-bit-bitfield-constant-conversion error:...
    
    ***
    
    PiperOrigin-RevId: 506466493

--- AppFramework/Event/GREYIOHIDEventTypes.h
+++ AppFramework/Event/GREYIOHIDEventTypes.h
@@ -259,5 +259,5 @@ void IOHIDEventSetIntegerValue(IOHIDEventRef hidEventRef, IOHIDEventField field,
  **/
 typedef struct {
   /** Internal flag required for touch injection. */
-  uint _firstTouchForView : 1;
+  char _firstTouchForView : 1;
 } UITouchFlags;
