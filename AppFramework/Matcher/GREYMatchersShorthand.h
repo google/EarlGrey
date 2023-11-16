734ec3251097ed7c47c66cc854e82cb1afef7878
Nishant Dalmia <nishantdalmia@google.com>
2023-11-16 12:36:16 -0800

    ObjC Style Compliance
    PiperOrigin-RevId: 583129998

--- AppFramework/Matcher/GREYMatchersShorthand.h
+++ AppFramework/Matcher/GREYMatchersShorthand.h
@@ -29,6 +29,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /** Shorthand for GREYMatchers::matcherForAccessibilityLabel:. */
 GREY_EXPORT id<GREYMatcher> grey_accessibilityLabel(NSString *label);
+GREY_EXPORT id<GREYMatcher> GREYAccessibilityLabel(NSString *label);
 
 /**
  * Shorthand for GREYMatchers::matcherForAccessibilityID:.
@@ -37,147 +38,192 @@ GREY_EXPORT id<GREYMatcher> grey_accessibilityLabel(NSString *label);
  *       testing.
  */
 GREY_EXPORT id<GREYMatcher> grey_accessibilityID(NSString *accessibilityID);
+GREY_EXPORT id<GREYMatcher> GREYAccessibilityID(NSString *accessibilityID);
 
 /** Shorthand for GREYMatchers::matcherForAccessibilityValue:. */
 GREY_EXPORT id<GREYMatcher> grey_accessibilityValue(NSString *grey_accessibilityValue);
+GREY_EXPORT id<GREYMatcher> GREYAccessibilityValue(NSString *grey_accessibilityValue);
 
 /** Shorthand for GREYMatchers::matcherForAccessibilityTraits:. */
 GREY_EXPORT id<GREYMatcher> grey_accessibilityTrait(UIAccessibilityTraits traits);
+GREY_EXPORT id<GREYMatcher> GREYAccessibilityTrait(UIAccessibilityTraits traits);
 
 /** Shorthand for GREYMatchers::matcherForAccessibilityElementIsFocused. */
 GREY_EXPORT id<GREYMatcher> grey_accessibilityFocused(void);
+GREY_EXPORT id<GREYMatcher> GREYAccessibilityFocused(void);
 
 /** Shorthand for GREYMatchers::matcherForAccessibilityHint:. */
 GREY_EXPORT id<GREYMatcher> grey_accessibilityHint(NSString *hint);
+GREY_EXPORT id<GREYMatcher> GREYAccessibilityHint(NSString *hint);
 
 /** Shorthand for GREYMatchers::matcherForAccessibilityElement. */
 GREY_EXPORT id<GREYMatcher> grey_accessibilityElement(void);
+GREY_EXPORT id<GREYMatcher> GREYAccessibilityElement(void);
 
 #pragma mark - UI Property Matchers
 
 /** Shorthand for GREYMatchers::matcherForButtonTitle:. */
 GREY_EXPORT id<GREYMatcher> grey_buttonTitle(NSString *text);
+GREY_EXPORT id<GREYMatcher> GREYButtonTitle(NSString *text);
 
 /** Shorthand for GREYMatchers::matcherForDatePickerValue:. */
 GREY_EXPORT id<GREYMatcher> grey_datePickerValue(NSDate *date);
+GREY_EXPORT id<GREYMatcher> GREYDatePickerValue(NSDate *date);
 
 /** Shorthand for GREYMatchers::matcherForEnabledElement. */
 GREY_EXPORT id<GREYMatcher> grey_enabled(void);
+GREY_EXPORT id<GREYMatcher> GREYEnabled(void);
 
 /** Shorthand for GREYMatchers::matcherForFirstResponder. */
 GREY_EXPORT id<GREYMatcher> grey_firstResponder(void);
+GREY_EXPORT id<GREYMatcher> GREYFirstResponder(void);
 
 /** Shorthand macro for GREYMatchers::matcherForHidden */
 GREY_EXPORT id<GREYMatcher> grey_hidden(BOOL hidden);
+GREY_EXPORT id<GREYMatcher> GREYHidden(BOOL hidden);
 
 /** Shorthand for GREYMatchers::matcherForInteractable. */
 GREY_EXPORT id<GREYMatcher> grey_interactable(void);
+GREY_EXPORT id<GREYMatcher> GREYInteractable(void);
 
 /** Shorthand for GREYMatchers::matcherForLayoutConstraints:toReferenceElementMatching:. */
 GREY_EXPORT id<GREYMatcher> grey_layout(NSArray *constraints,
                                         id<GREYMatcher> referenceElementMatcher);
+GREY_EXPORT id<GREYMatcher> GREYLayout(NSArray *constraints,
+                                       id<GREYMatcher> referenceElementMatcher);
 
 /** Shorthand for GREYMatchers::matcherForMinimumVisiblePercent:. */
 GREY_EXPORT id<GREYMatcher> grey_minimumVisiblePercent(CGFloat percent);
+GREY_EXPORT id<GREYMatcher> GREYMinimumVisiblePercent(CGFloat percent);
 
 /** Shorthand for GREYMatchers::matcherForNotVisible. */
 GREY_EXPORT id<GREYMatcher> grey_notVisible(void);
+GREY_EXPORT id<GREYMatcher> GREYNotVisible(void);
 
 /** Shorthand for GREYMatchers::matcherForPickerColumn:setToValue:. */
 GREY_EXPORT id<GREYMatcher> grey_pickerColumnSetToValue(NSInteger column,
                                                         NSString *_Nullable value);
+GREY_EXPORT id<GREYMatcher> GREYPickerColumnSetToValue(NSInteger column, NSString *_Nullable value);
 
 /** Shorthand for GREYMatchers::matcherForProgress:. */
 GREY_EXPORT id<GREYMatcher> grey_progress(id<GREYMatcher> comparisonMatcher);
+GREY_EXPORT id<GREYMatcher> GREYProgress(id<GREYMatcher> comparisonMatcher);
 
 /** Shorthand for GREYMatchers::matcherForScrolledToContentEdge:. */
 GREY_EXPORT id<GREYMatcher> grey_scrolledToContentEdge(GREYContentEdge edge);
+GREY_EXPORT id<GREYMatcher> GREYScrolledToContentEdge(GREYContentEdge edge);
 
 /** Shorthand for GREYMatchers::matcherForScrollViewContentOffset:. */
 GREY_EXPORT id<GREYMatcher> grey_scrollViewContentOffset(CGPoint offset);
+GREY_EXPORT id<GREYMatcher> GREYScrollViewContentOffset(CGPoint offset);
 
 /** Shorthand for GREYMatchers::matcherForSelectedElement. */
 GREY_EXPORT id<GREYMatcher> grey_selected(void);
+GREY_EXPORT id<GREYMatcher> GREYSelected(void);
 
 /** Shorthand for GREYMatchers::matcherForSliderValueMatcher:. */
 GREY_EXPORT id<GREYMatcher> grey_sliderValueMatcher(id<GREYMatcher> valueMatcher);
+GREY_EXPORT id<GREYMatcher> GREYSliderValueMatcher(id<GREYMatcher> valueMatcher);
 
 /** Shorthand for GREYMatchers::matcherForStepperValue:. */
 GREY_EXPORT id<GREYMatcher> grey_stepperValue(double value);
+GREY_EXPORT id<GREYMatcher> GREYStepperValue(double value);
 
 /** Shorthand for GREYMatchers::matcherForSufficientlyVisible. */
 GREY_EXPORT id<GREYMatcher> grey_sufficientlyVisible(void);
+GREY_EXPORT id<GREYMatcher> GREYSufficientlyVisible(void);
 
 /** Shorthand for GREYMatchers::matcherForSwitchWithOnState:. */
 GREY_EXPORT id<GREYMatcher> grey_switchWithOnState(BOOL on);
+GREY_EXPORT id<GREYMatcher> GREYSwitchWithOnState(BOOL on);
 
 /** Shorthand for GREYMatchers::matcherForText:. */
 GREY_EXPORT id<GREYMatcher> grey_text(NSString *inputText);
+GREY_EXPORT id<GREYMatcher> GREYText(NSString *inputText);
 
 /** Shorthand for GREYMatchers::matcherForTextFieldValue:. */
 GREY_EXPORT id<GREYMatcher> grey_textFieldValue(NSString *value);
+GREY_EXPORT id<GREYMatcher> GREYTextFieldValue(NSString *value);
 
 /** Shorthand for GREYMatchers::matcherForUserInteractionEnabled. */
 GREY_EXPORT id<GREYMatcher> grey_userInteractionEnabled(void);
+GREY_EXPORT id<GREYMatcher> GREYUserInteractionEnabled(void);
 
 #pragma mark - Hierarchy Based Matchers
 
 /** Shorthand for GREYMatchers::matcherForAncestor:. */
 GREY_EXPORT id<GREYMatcher> grey_ancestor(id<GREYMatcher> ancestorMatcher);
+GREY_EXPORT id<GREYMatcher> GREYAncestor(id<GREYMatcher> ancestorMatcher);
 
 /** Shorthand for GREYMatchers::matcherForDescendant:. */
 GREY_EXPORT id<GREYMatcher> grey_descendant(id<GREYMatcher> descendantMatcher);
+GREY_EXPORT id<GREYMatcher> GREYDescendant(id<GREYMatcher> descendantMatcher);
 
 /** Shorthand for GREYMatchers::matcherForSubview. */
 GREY_EXPORT id<GREYMatcher> grey_subview(id<GREYMatcher> subviewMatcher);
+GREY_EXPORT id<GREYMatcher> GREYSubview(id<GREYMatcher> subviewMatcher);
 
 /** Shorthand for GREYMatchers::matcherForSibling. */
 GREY_EXPORT id<GREYMatcher> grey_sibling(id<GREYMatcher> siblingMatcher);
+GREY_EXPORT id<GREYMatcher> GREYSibling(id<GREYMatcher> siblingMatcher);
 
 /** Shorthand for GREYMatchers::matcherForKeyWindow. */
 GREY_EXPORT id<GREYMatcher> grey_keyWindow(void);
+GREY_EXPORT id<GREYMatcher> GREYKeyWindow(void);
 
 #pragma mark - Class-type Based Matchers
 
 /** Shorthand for GREYMatchers::matcherForConformsToProtocol:. */
 GREY_EXPORT id<GREYMatcher> grey_conformsToProtocol(Protocol *protocol);
+GREY_EXPORT id<GREYMatcher> GREYConformsToProtocol(Protocol *protocol);
 
 /** Shorthand for GREYMatchers::matcherForKindOfClass:. */
 GREY_EXPORT id<GREYMatcher> grey_kindOfClass(Class _Nullable klass);
+GREY_EXPORT id<GREYMatcher> GREYKindOfClass(Class _Nullable klass);
 
 /** Shorthand for GREYMatchers::matcherForKindOfClassName:. */
 GREY_EXPORT id<GREYMatcher> grey_kindOfClassName(NSString *className);
+GREY_EXPORT id<GREYMatcher> GREYKindOfClassName(NSString *className);
 
 /** Shorthand for GREYMatchers::matcherForRespondsToSelector:. */
 GREY_EXPORT id<GREYMatcher> grey_respondsToSelector(SEL sel);
+GREY_EXPORT id<GREYMatcher> GREYRespondsToSelector(SEL sel);
 
 // Comparison based matchers
 
 /** Shorthand for GREYMatchers::matcherForCloseTo:. */
 GREY_EXPORT id<GREYMatcher> grey_closeTo(double value, double delta);
+GREY_EXPORT id<GREYMatcher> GREYCloseTo(double value, double delta);
 
 /** Shorthand for GREYMatchers::matcherForEqualTo:. */
 GREY_EXPORT id<GREYMatcher> grey_equalTo(id value);
+GREY_EXPORT id<GREYMatcher> GREYEqualTo(id value);
 
 /** Shorthand for GREYMatchers::matcherForLessThan:. */
 GREY_EXPORT id<GREYMatcher> grey_lessThan(id value);
+GREY_EXPORT id<GREYMatcher> GREYLessThan(id value);
 
 /** Shorthand for GREYMatchers::matcherForGreaterThan:. */
 GREY_EXPORT id<GREYMatcher> grey_greaterThan(id value);
+GREY_EXPORT id<GREYMatcher> GREYGreaterThan(id value);
 
 #pragma mark - Other Matchers
 
 /** Shorthand for GREYMatchers::matcherForAnything. */
 GREY_EXPORT id<GREYMatcher> grey_anything(void);
+GREY_EXPORT id<GREYMatcher> GREYAnything(void);
 
 /** Shorthand for GREYMatchers::matcherForNil. */
 GREY_EXPORT id<GREYMatcher> grey_nil(void);
+GREY_EXPORT id<GREYMatcher> GREYNil(void);
 
 /** Shorthand for GREYMatchers::matcherForNotNil. */
 GREY_EXPORT id<GREYMatcher> grey_notNil(void);
+GREY_EXPORT id<GREYMatcher> GREYNotNil(void);
 
 /** Shorthand for GREYMatchers::matcherForSystemAlertViewShown. */
 GREY_EXPORT id<GREYMatcher> grey_systemAlertViewShown(void);
+GREY_EXPORT id<GREYMatcher> GREYSystemAlertViewShown(void);
 
 #pragma mark - Clubbing / Logic Matchers
 
@@ -209,6 +255,7 @@ GREY_EXPORT id<GREYMatcher> grey_anyOf(id<GREYMatcher> match, ...)
 
 /** Shorthand for GREYMatchers::matcherForNegation:. */
 GREY_EXPORT id<GREYMatcher> grey_not(id<GREYMatcher> matcher);
+GREY_EXPORT id<GREYMatcher> GREYNot(id<GREYMatcher> matcher);
 
 // Clubbing / Logic Matchers (Swift-Based)
 
@@ -222,6 +269,8 @@ GREY_EXPORT id<GREYMatcher> grey_not(id<GREYMatcher> matcher);
  */
 GREY_EXPORT id<GREYMatcher> grey_allOfMatchers(NSArray<__kindof id<GREYMatcher>> *matchers)
     NS_SWIFT_NAME(grey_allOf(_:));
+GREY_EXPORT id<GREYMatcher> GREYAllOfMatchers(NSArray<__kindof id<GREYMatcher>> *matchers)
+    NS_SWIFT_NAME(GREYAllOf(_:));
 
 /**
  * A matcher that is a logical OR of all the matchers passed in within an NSArray.
@@ -233,6 +282,8 @@ GREY_EXPORT id<GREYMatcher> grey_allOfMatchers(NSArray<__kindof id<GREYMatcher>>
  */
 GREY_EXPORT id<GREYMatcher> grey_anyOfMatchers(NSArray<__kindof id<GREYMatcher>> *matchers)
     NS_SWIFT_NAME(grey_anyOf(_:));
+GREY_EXPORT id<GREYMatcher> GREYAnyOfMatchers(NSArray<__kindof id<GREYMatcher>> *matchers)
+    NS_SWIFT_NAME(GREYAnyOf(_:));
 
 #endif  // GREY_DISABLE_SHORTHAND
 
--- AppFramework/Matcher/GREYMatchersShorthand.m
+++ AppFramework/Matcher/GREYMatchersShorthand.m
@@ -25,148 +25,211 @@
 
 #if !defined(GREY_DISABLE_SHORTHAND) || !(GREY_DISABLE_SHORTHAND)
 
-id<GREYMatcher> grey_keyWindow(void) { return [GREYMatchers matcherForKeyWindow]; }
+id<GREYMatcher> GREYKeyWindow(void) { return [GREYMatchers matcherForKeyWindow]; }
+id<GREYMatcher> grey_keyWindow(void) { return GREYKeyWindow(); }
 
-id<GREYMatcher> grey_accessibilityLabel(NSString *label) {
+id<GREYMatcher> GREYAccessibilityLabel(NSString *label) {
   return [GREYMatchers matcherForAccessibilityLabel:label];
 }
+id<GREYMatcher> grey_accessibilityLabel(NSString *label) { return GREYAccessibilityLabel(label); }
 
-id<GREYMatcher> grey_accessibilityID(NSString *accessibilityID) {
+id<GREYMatcher> GREYAccessibilityID(NSString *accessibilityID) {
   return [GREYMatchers matcherForAccessibilityID:accessibilityID];
 }
-
-id<GREYMatcher> grey_accessibilityValue(NSString *value) {
-  return [GREYMatchers matcherForAccessibilityValue:value];
+id<GREYMatcher> grey_accessibilityID(NSString *accessibilityID) {
+  return GREYAccessibilityID(accessibilityID);
 }
 
-id<GREYMatcher> grey_accessibilityTrait(UIAccessibilityTraits traits) {
+id<GREYMatcher> GREYAccessibilityValue(NSString *value) {
+  return [GREYMatchers matcherForAccessibilityValue:value];
+}
+id<GREYMatcher> grey_accessibilityValue(NSString *value) { return GREYAccessibilityValue(value); }
+
+id<GREYMatcher> GREYAccessibilityTrait(UIAccessibilityTraits traits) {
   return [GREYMatchers matcherForAccessibilityTraits:traits];
 }
 
-id<GREYMatcher> grey_accessibilityHint(NSString *hint) {
+id<GREYMatcher> grey_accessibilityTrait(UIAccessibilityTraits traits) {
+  return GREYAccessibilityTrait(traits);
+}
+
+id<GREYMatcher> GREYAccessibilityHint(NSString *hint) {
   return [GREYMatchers matcherForAccessibilityHint:hint];
 }
+id<GREYMatcher> grey_accessibilityHint(NSString *hint) { return GREYAccessibilityHint(hint); }
 
-id<GREYMatcher> grey_accessibilityFocused(void) {
+id<GREYMatcher> GREYAccessibilityFocused(void) {
   return [GREYMatchers matcherForAccessibilityElementIsFocused];
 }
+id<GREYMatcher> grey_accessibilityFocused(void) { return GREYAccessibilityFocused(); }
 
-id<GREYMatcher> grey_text(NSString *text) { return [GREYMatchers matcherForText:text]; }
+id<GREYMatcher> GREYText(NSString *text) { return [GREYMatchers matcherForText:text]; }
+id<GREYMatcher> grey_text(NSString *text) { return GREYText(text); }
 
-id<GREYMatcher> grey_firstResponder(void) { return [GREYMatchers matcherForFirstResponder]; }
+id<GREYMatcher> GREYFirstResponder(void) { return [GREYMatchers matcherForFirstResponder]; }
+id<GREYMatcher> grey_firstResponder(void) { return GREYFirstResponder(); }
 
-id<GREYMatcher> grey_minimumVisiblePercent(CGFloat percent) {
+id<GREYMatcher> GREYMinimumVisiblePercent(CGFloat percent) {
   return [GREYMatchers matcherForMinimumVisiblePercent:percent];
 }
+id<GREYMatcher> grey_minimumVisiblePercent(CGFloat percent) {
+  return GREYMinimumVisiblePercent(percent);
+}
 
-id<GREYMatcher> grey_sufficientlyVisible(void) {
+id<GREYMatcher> GREYSufficientlyVisible(void) {
   return [GREYMatchers matcherForSufficientlyVisible];
 }
+id<GREYMatcher> grey_sufficientlyVisible(void) { return GREYSufficientlyVisible(); }
 
-id<GREYMatcher> grey_notVisible(void) { return [GREYMatchers matcherForNotVisible]; }
+id<GREYMatcher> GREYNotVisible(void) { return [GREYMatchers matcherForNotVisible]; }
+id<GREYMatcher> grey_notVisible(void) { return GREYNotVisible(); }
 
-id<GREYMatcher> grey_interactable(void) { return [GREYMatchers matcherForInteractable]; }
+id<GREYMatcher> GREYInteractable(void) { return [GREYMatchers matcherForInteractable]; }
+id<GREYMatcher> grey_interactable(void) { return GREYInteractable(); }
 
-id<GREYMatcher> grey_accessibilityElement(void) {
+id<GREYMatcher> GREYAccessibilityElement(void) {
   return [GREYMatchers matcherForAccessibilityElement];
 }
+id<GREYMatcher> grey_accessibilityElement(void) { return GREYAccessibilityElement(); }
 
-id<GREYMatcher> grey_kindOfClass(Class klass) { return [GREYMatchers matcherForKindOfClass:klass]; }
+id<GREYMatcher> GREYKindOfClass(Class klass) { return [GREYMatchers matcherForKindOfClass:klass]; }
+id<GREYMatcher> grey_kindOfClass(Class klass) { return GREYKindOfClass(klass); }
 
-id<GREYMatcher> grey_kindOfClassName(NSString *className) {
+id<GREYMatcher> GREYKindOfClassName(NSString *className) {
   return [GREYMatchers matcherForKindOfClassName:className];
 }
+id<GREYMatcher> grey_kindOfClassName(NSString *className) { return GREYKindOfClassName(className); }
 
-id<GREYMatcher> grey_progress(id<GREYMatcher> comparisonMatcher) {
+id<GREYMatcher> GREYProgress(id<GREYMatcher> comparisonMatcher) {
   return [GREYMatchers matcherForProgress:comparisonMatcher];
 }
+id<GREYMatcher> grey_progress(id<GREYMatcher> comparisonMatcher) {
+  return GREYProgress(comparisonMatcher);
+}
 
-id<GREYMatcher> grey_respondsToSelector(SEL sel) {
+id<GREYMatcher> GREYRespondsToSelector(SEL sel) {
   return [GREYMatchers matcherForRespondsToSelector:sel];
 }
+id<GREYMatcher> grey_respondsToSelector(SEL sel) { return GREYRespondsToSelector(sel); }
 
-id<GREYMatcher> grey_conformsToProtocol(Protocol *protocol) {
+id<GREYMatcher> GREYConformsToProtocol(Protocol *protocol) {
   return [GREYMatchers matcherForConformsToProtocol:protocol];
 }
+id<GREYMatcher> grey_conformsToProtocol(Protocol *protocol) {
+  return GREYConformsToProtocol(protocol);
+}
 
-id<GREYMatcher> grey_ancestor(id<GREYMatcher> matcher) {
+id<GREYMatcher> GREYAncestor(id<GREYMatcher> matcher) {
   return [GREYMatchers matcherForAncestor:matcher];
 }
+id<GREYMatcher> grey_ancestor(id<GREYMatcher> matcher) { return GREYAncestor(matcher); }
 
-id<GREYMatcher> grey_descendant(id<GREYMatcher> matcher) {
+id<GREYMatcher> GREYDescendant(id<GREYMatcher> matcher) {
   return [GREYMatchers matcherForDescendant:matcher];
 }
+id<GREYMatcher> grey_descendant(id<GREYMatcher> matcher) { return GREYDescendant(matcher); }
 
-id<GREYMatcher> grey_buttonTitle(NSString *text) {
+id<GREYMatcher> GREYButtonTitle(NSString *text) {
   return [GREYMatchers matcherForButtonTitle:text];
 }
+id<GREYMatcher> grey_buttonTitle(NSString *text) { return GREYButtonTitle(text); }
 
-id<GREYMatcher> grey_scrollViewContentOffset(CGPoint offset) {
+id<GREYMatcher> GREYScrollViewContentOffset(CGPoint offset) {
   return [GREYMatchers matcherForScrollViewContentOffset:offset];
 }
+id<GREYMatcher> grey_scrollViewContentOffset(CGPoint offset) {
+  return GREYScrollViewContentOffset(offset);
+}
 
-id<GREYMatcher> grey_sliderValueMatcher(id<GREYMatcher> valueMatcher) {
+id<GREYMatcher> GREYSliderValueMatcher(id<GREYMatcher> valueMatcher) {
   return [GREYMatchers matcherForSliderValueMatcher:valueMatcher];
 }
+id<GREYMatcher> grey_sliderValueMatcher(id<GREYMatcher> valueMatcher) {
+  return GREYSliderValueMatcher(valueMatcher);
+}
 
-id<GREYMatcher> grey_stepperValue(double value) {
+id<GREYMatcher> GREYStepperValue(double value) {
   return [GREYMatchers matcherForStepperValue:value];
 }
+id<GREYMatcher> grey_stepperValue(double value) { return GREYStepperValue(value); }
 
-id<GREYMatcher> grey_pickerColumnSetToValue(NSInteger column, NSString *value) {
+id<GREYMatcher> GREYPickerColumnSetToValue(NSInteger column, NSString *value) {
   return [GREYMatchers matcherForPickerColumn:column setToValue:value];
 }
+id<GREYMatcher> grey_pickerColumnSetToValue(NSInteger column, NSString *value) {
+  return GREYPickerColumnSetToValue(column, value);
+}
 
-id<GREYMatcher> grey_systemAlertViewShown(void) {
+id<GREYMatcher> GREYSystemAlertViewShown(void) {
   return [GREYMatchers matcherForSystemAlertViewShown];
 }
+id<GREYMatcher> grey_systemAlertViewShown(void) { return GREYSystemAlertViewShown(); }
 
-id<GREYMatcher> grey_datePickerValue(NSDate *value) {
+id<GREYMatcher> GREYDatePickerValue(NSDate *value) {
   return [GREYMatchers matcherForDatePickerValue:value];
 }
+id<GREYMatcher> grey_datePickerValue(NSDate *value) { return GREYDatePickerValue(value); }
 
-id<GREYMatcher> grey_enabled(void) { return [GREYMatchers matcherForEnabledElement]; }
+id<GREYMatcher> GREYEnabled(void) { return [GREYMatchers matcherForEnabledElement]; }
+id<GREYMatcher> grey_enabled(void) { return GREYEnabled(); }
 
-id<GREYMatcher> grey_selected(void) { return [GREYMatchers matcherForSelectedElement]; }
+id<GREYMatcher> GREYSelected(void) { return [GREYMatchers matcherForSelectedElement]; }
+id<GREYMatcher> grey_selected(void) { return GREYSelected(); }
 
-id<GREYMatcher> grey_userInteractionEnabled(void) {
+id<GREYMatcher> GREYUserInteractionEnabled(void) {
   return [GREYMatchers matcherForUserInteractionEnabled];
 }
+id<GREYMatcher> grey_userInteractionEnabled(void) { return GREYUserInteractionEnabled(); }
 
-id<GREYMatcher> grey_layout(NSArray *constraints, id<GREYMatcher> referenceElementMatcher) {
+id<GREYMatcher> GREYLayout(NSArray *constraints, id<GREYMatcher> referenceElementMatcher) {
   NSArray<GREYLayoutConstraint *> *appConstraints =
       GREYIsTestProcess() ? [constraints passByValue] : constraints;
   return [GREYMatchers matcherForLayoutConstraints:appConstraints
                         toReferenceElementMatching:referenceElementMatcher];
 }
+id<GREYMatcher> grey_layout(NSArray *constraints, id<GREYMatcher> referenceElementMatcher) {
+  return GREYLayout(constraints, referenceElementMatcher);
+}
 
-id<GREYMatcher> grey_nil(void) { return [GREYMatchers matcherForNil]; }
+id<GREYMatcher> GREYNil(void) { return [GREYMatchers matcherForNil]; }
+id<GREYMatcher> grey_nil(void) { return GREYNil(); }
 
-id<GREYMatcher> grey_notNil(void) { return [GREYMatchers matcherForNotNil]; }
+id<GREYMatcher> GREYNotNil(void) { return [GREYMatchers matcherForNotNil]; }
+id<GREYMatcher> grey_notNil(void) { return GREYNotNil(); }
 
-id<GREYMatcher> grey_switchWithOnState(BOOL on) {
+id<GREYMatcher> GREYSwitchWithOnState(BOOL on) {
   return [GREYMatchers matcherForSwitchWithOnState:on];
 }
+id<GREYMatcher> grey_switchWithOnState(BOOL on) { return GREYSwitchWithOnState(on); }
 
-id<GREYMatcher> grey_closeTo(double value, double delta) {
+id<GREYMatcher> GREYCloseTo(double value, double delta) {
   return [GREYMatchers matcherForCloseTo:value delta:delta];
 }
+id<GREYMatcher> grey_closeTo(double value, double delta) { return GREYCloseTo(value, delta); }
 
-id<GREYMatcher> grey_anything(void) { return [GREYMatchers matcherForAnything]; }
+id<GREYMatcher> GREYAnything(void) { return [GREYMatchers matcherForAnything]; }
+id<GREYMatcher> grey_anything(void) { return GREYAnything(); }
 
-id<GREYMatcher> grey_equalTo(id value) { return [GREYMatchers matcherForEqualTo:value]; }
+id<GREYMatcher> GREYEqualTo(id value) { return [GREYMatchers matcherForEqualTo:value]; }
+id<GREYMatcher> grey_equalTo(id value) { return GREYEqualTo(value); }
 
-id<GREYMatcher> grey_lessThan(id value) { return [GREYMatchers matcherForLessThan:value]; }
+id<GREYMatcher> GREYLessThan(id value) { return [GREYMatchers matcherForLessThan:value]; }
+id<GREYMatcher> grey_lessThan(id value) { return GREYLessThan(value); }
 
-id<GREYMatcher> grey_greaterThan(id value) { return [GREYMatchers matcherForGreaterThan:value]; }
+id<GREYMatcher> GREYGreaterThan(id value) { return [GREYMatchers matcherForGreaterThan:value]; }
+id<GREYMatcher> grey_greaterThan(id value) { return GREYGreaterThan(value); }
 
-id<GREYMatcher> grey_scrolledToContentEdge(GREYContentEdge edge) {
+id<GREYMatcher> GREYScrolledToContentEdge(GREYContentEdge edge) {
   return [GREYMatchers matcherForScrolledToContentEdge:edge];
 }
+id<GREYMatcher> grey_scrolledToContentEdge(GREYContentEdge edge) {
+  return GREYScrolledToContentEdge(edge);
+}
 
-id<GREYMatcher> grey_textFieldValue(NSString *value) {
+id<GREYMatcher> GREYTextFieldValue(NSString *value) {
   return [GREYMatchers matcherForTextFieldValue:value];
 }
+id<GREYMatcher> grey_textFieldValue(NSString *value) { return GREYTextFieldValue(value); }
 
 #pragma mark - Compound Matchers.
 
@@ -181,13 +244,16 @@ id<GREYMatcher> grey_anyOf(id<GREYMatcher> matcher, ...) {
   } while ((nextMatcher = va_arg(args, id<GREYMatcher>)) != nil);
 
   va_end(args);
-  return grey_anyOfMatchers(matcherList);
+  return GREYAnyOfMatchers(matcherList);
 }
 
-id<GREYMatcher> grey_anyOfMatchers(NSArray<GREYMatcher> *matchers) {
+id<GREYMatcher> GREYAnyOfMatchers(NSArray<GREYMatcher> *matchers) {
   NSArray<id<GREYMatcher>> *appMatchers =
       GREYIsTestProcess() ? GREYGetRemoteArrayShallowCopy(matchers) : matchers;
   return [[GREYAnyOf alloc] initWithMatchers:appMatchers];
+}
+id<GREYMatcher> grey_anyOfMatchers(NSArray<GREYMatcher> *matchers) {
+  return GREYAnyOfMatchers(matchers);
 }
 
 id<GREYMatcher> grey_allOf(id<GREYMatcher> matcher, ...) {
@@ -201,26 +267,33 @@ id<GREYMatcher> grey_allOf(id<GREYMatcher> matcher, ...) {
   } while ((nextMatcher = va_arg(args, id<GREYMatcher>)) != nil);
 
   va_end(args);
-  return grey_allOfMatchers(matcherList);
+  return GREYAllOfMatchers(matcherList);
 }
 
-id<GREYMatcher> grey_allOfMatchers(NSArray<GREYMatcher> *matchers) {
+id<GREYMatcher> GREYAllOfMatchers(NSArray<GREYMatcher> *matchers) {
   NSArray<id<GREYMatcher>> *appMatchers =
       GREYIsTestProcess() ? GREYGetRemoteArrayShallowCopy(matchers) : matchers;
   return [[GREYAllOf alloc] initWithMatchers:appMatchers];
 }
+id<GREYMatcher> grey_allOfMatchers(NSArray<GREYMatcher> *matchers) {
+  return GREYAllOfMatchers(matchers);
+}
 
-id<GREYMatcher> grey_not(id<GREYMatcher> matcher) {
+id<GREYMatcher> GREYNot(id<GREYMatcher> matcher) {
   return [GREYMatchers matcherForNegation:matcher];
 }
+id<GREYMatcher> grey_not(id<GREYMatcher> matcher) { return GREYNot(matcher); }
 
-id<GREYMatcher> grey_hidden(BOOL hidden) { return [GREYMatchers matcherForHidden:hidden]; }
+id<GREYMatcher> GREYHidden(BOOL hidden) { return [GREYMatchers matcherForHidden:hidden]; }
+id<GREYMatcher> grey_hidden(BOOL hidden) { return GREYHidden(hidden); }
 
-id<GREYMatcher> grey_subview(id<GREYMatcher> matcher) {
+id<GREYMatcher> GREYSubview(id<GREYMatcher> matcher) {
   return [GREYMatchers matcherForSubview:matcher];
 }
+id<GREYMatcher> grey_subview(id<GREYMatcher> matcher) { return GREYSubview(matcher); }
 
-id<GREYMatcher> grey_sibling(id<GREYMatcher> matcher) {
+id<GREYMatcher> GREYSibling(id<GREYMatcher> matcher) {
   return [GREYMatchers matcherForSibling:matcher];
 }
+id<GREYMatcher> grey_sibling(id<GREYMatcher> matcher) { return GREYSibling(matcher); }
 #endif  // GREY_DISABLE_SHORTHAND
