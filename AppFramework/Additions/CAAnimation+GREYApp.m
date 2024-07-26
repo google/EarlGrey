8504ed1f0137efe021f1dab0ae8197bad54dbdbf
Justin Cohen <justincohen@google.com>
2024-07-26 15:50:54 -0400

    Experiment with a UIPresentationController tracker. Embed within the CAAnimation file to simplify testing.

--- AppFramework/Additions/CAAnimation+GREYApp.m
+++ AppFramework/Additions/CAAnimation+GREYApp.m
@@ -103,3 +103,56 @@
 }
 
 @end
+
+@interface UIPresentationController (Private)
+
+- (int)transitionDidFinish:(int)arg2;
+- (int)runTransitionForCurrentStateAnimated:(int)arg2 handoffData:(id)arg3;
+
+@end
+
+@implementation UIPresentationController (GREYApp)
+
++ (void)load {
+  if (@available(iOS 18, *)) {
+    GREYSwizzler* swizzler = [[GREYSwizzler alloc] init];
+    BOOL swizzleSuccess =
+        [swizzler swizzleClass:self
+            replaceInstanceMethod:@selector(runTransitionForCurrentStateAnimated:handoffData:)
+                       withMethod:@selector
+                       (greyswizzled_runTransitionForCurrentStateAnimated:handoffData:)];
+    GREYFatalAssertWithMessage(
+        swizzleSuccess, @"Cannot swizzle UIPresentationController "
+                        @"runTransitionForCurrentStateAnimated:handoffData:");
+    swizzleSuccess =
+        [swizzler swizzleClass:self
+            replaceInstanceMethod:@selector(transitionDidFinish:)
+                       withMethod:@selector(greyswizzled_transitionDidFinish:)];
+    GREYFatalAssertWithMessage(
+        swizzleSuccess,
+        @"Cannot swizzle UIPresentationController transitionDidFinish:");
+   }
+}
+
+- (int)greyswizzled_runTransitionForCurrentStateAnimated:(int)state handoffData:(id)data {
+  GREYAppStateTrackerObject* object =
+      TRACK_STATE_FOR_OBJECT(kGREYPendingGestureRecognition, self);
+  objc_setAssociatedObject(self, @selector(greyswizzled_transitionDidFinish:),
+                           object, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+  return INVOKE_ORIGINAL_IMP2(
+      int,
+      @selector(greyswizzled_runTransitionForCurrentStateAnimated:handoffData:),
+      state, data);
+}
+
+- (int)greyswizzled_transitionDidFinish:(int)state {
+  GREYAppStateTrackerObject* object = objc_getAssociatedObject(
+      self, @selector(greyswizzled_transitionDidFinish:));
+  UNTRACK_STATE_FOR_OBJECT(kGREYPendingGestureRecognition, object);
+  objc_setAssociatedObject(self, @selector(greyswizzled_transitionDidFinish:),
+                           nil, OBJC_ASSOCIATION_ASSIGN);
+  return INVOKE_ORIGINAL_IMP1(int, @selector(greyswizzled_transitionDidFinish:),
+                              state);
+}
+
+@end
