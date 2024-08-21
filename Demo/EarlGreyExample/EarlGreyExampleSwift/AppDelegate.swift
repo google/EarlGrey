dff18cd48a26a4b12fc2a79a24526c3bbb5a9cd8
Tony Allevato <allevato@google.com>
2024-08-21 12:01:31 -0700

    Internal Commit Uploaded
    PiperOrigin-RevId: 665974712

--- Demo/EarlGreyExample/EarlGreyExampleSwift/AppDelegate.swift
+++ Demo/EarlGreyExample/EarlGreyExampleSwift/AppDelegate.swift
@@ -16,13 +16,14 @@
 
 import UIKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
-  func application(_ application: UIApplication,
-      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+  ) -> Bool {
     return true
   }
 }
-
